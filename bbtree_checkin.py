8a0d5727e9e4a116585d3d670af130ef2bb6cf98
raxxar1024 <yjw013809@gmail.com>
2017-04-11 15:35:55 +0800

    签到

--- bbtree_checkin.py
+++ bbtree_checkin.py
@@ -4,34 +4,49 @@ import cookielib
 import simplejson
 import urllib2
 import urllib
+import json
 
 
-m_host = "https://pro.zhihuishu.bbtree.com/service/v2/user/login_v5"
+# m_host = "https://pro.zhihuishu.bbtree.com/service/v2/user/login_v5"
+m_host = "https://pro.zhihuishu.bbtree.com/service/v2/user/sigin_in"
 
 data_json = {
-    "data": "Sf/LZAiec/D6yRc9ftk7hcTM7WAyvqYiAbTF028B6igLJ6eCw1TVqlEkCyp0gVLEPZOsYzIt1Yvz1HoTDx1zXIObYyDmVCTWRN/M1UZ7npfQR2Cl69PUs+NC4dXtjKnwdrqDDPpLTGFSq/+vTQeORXW5VFY1gqBsqATKOJW2dbC9rhzwC3xaQQhX5u3pC1IAh3rshxTiqFcSMjIvI18oQAhKRVJLIDJ+aYB7zg8DMExcfmFYBUQ3OiLANNu4Dsm51JdwwnZmylXu2hjMPaQxCAFBLwT+psk+OXd70w2f5JaCpV9FFht5j9fqqUOH93gA1wjV2J0S2gkrYga9KO4TeY8B1/073STFNrvAvAGnoLuba+4o6TyGpTx0gabrxpGlmylotv5bzZv24eIt0UMEBA==",
+    "data": "gaYP/g3PUh5xMy432Y3LiDasGu2fzfIyuoD1GTk+SzWhgkKJ1L"
+            "dLWjnZUHRLt/b+FI8iBDY9n+dLu7f6ojvfJ//2Omry/n/0OuLgm"
+            "fYzoOb0BitUFHsI18Zl5bRxu4QR1wjV2J0S2gkrYga9KO4TeSHf"
+            "aMyS2VSY5r++XUuVWQkq7KYdublbyxH90q+EhXoiaOGC/SGv3Hn"
+            "46hV7BzR2XVq+sGSbIljtoIJfhzPdtMPaNTYM3jK3iGdSRA3Qw5wo"
+            "5eq6HevAY5UZCjyxHytsvWVhD5Eb252uUuoVKb0CoSLj/sZi03wsc0"
+            "bV1hefsSXmsYZoR5x7gX7YQyggwS0vl8cseSrkOs0mWxLopAz3C6s=",
     "data_ver": 34,
     "ios_arm64_flag": 1,
     "uuid": "d492dd329a0cd0624df8ced1c741202a"
 }
+# post_data = urllib.urlencode(data_json)   # 将post消息化成可以让服务器编码的方式
+# post_data = str(data_json)   # 将post消息化成可以让服务器编码的方式
 data_string = simplejson.dumps(data_json)
 
 post_header = {
     "User-Agent": "WisdomTree/6.0.1 (iPhone; iOS 10.3.1; Scale/2.00)",
-    "Content-Length": str(len(data_string)),
+    # "Content-Length": str(len(data_string)),
     "Content-Type": "application/json",
     "Accept-Language": "zh-Hans-CN;q=1",
-    "Accept-Encoding": "gzip, deflate"
+    "Accept-Encoding": "gzip, deflate",
+    "Accept": "*/*"
 }
 
-# login
-post_data = urllib.urlencode(data_json)   # 将post消息化成可以让服务器编码的方式
-opener = urllib2.build_opener()
-req = urllib2.Request(m_host, post_data, post_header)
-content = opener.open(req)
-print content.read()
-
 # checkin
+# req = urllib2.Request(m_host, data_string, post_header)
+# response_stream = urllib2.urlopen(req)
+# res = response_stream.read()
+# print(res)
+
+params = json.dumps(data_json)
+print params
+f = urllib.urlopen(m_host, params, post_header)
+rsp = f.read()
+print(rsp)
+
 
 
 
