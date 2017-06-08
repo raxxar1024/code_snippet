9f594e20b6ad64315d6606398d1112067c766712
raxxar1024 <yjw013809@gmail.com>
2017-06-08 09:52:37 +0800

    stem of 3. Longest Substring Without Repeating Characters

---
+++ leetcode/3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+if __name__ == "__main__":
+    assert Solution().lengthOfLongestSubstring("abcabcbb") == 3
+    assert Solution().lengthOfLongestSubstring("bbbbb") == 1
+    assert Solution().lengthOfLongestSubstring("pwwkew") == 3
+    
