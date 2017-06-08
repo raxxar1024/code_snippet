9d4dce369fb7db2bcee99453d761efad82668822
raxxar1024 <yjw013809@gmail.com>
2017-06-08 09:39:52 +0800

    optimize main

--- leetcode/2.Add Two Numbers.py
+++ leetcode/2.Add Two Numbers.py
@@ -42,28 +42,21 @@ class Solution(object):
         return result.next
 
 if __name__ == "__main__":
-    l1 = ListNode(2)
-    l1.next = ListNode(4)
-    l1.next.next = ListNode(3)
-    l2 = ListNode(5)
-    l2.next = ListNode(6)
-    l2.next.next = ListNode(4)
-    # l1 = [2, 4, 3]
-    # l2 = [5, 6, 4]
-    s = Solution()
-    result = s.addTwoNumbers(l1, l2)
+    l1, l1.next, l1.next.next = ListNode(2), ListNode(4), ListNode(3)
+    l2, l2.next, l2.next.next = ListNode(5), ListNode(6), ListNode(4)
+
+    result = Solution().addTwoNumbers(l1, l2)
     assert result.val == 7
     assert result.next.val == 0
     assert result.next.next.val == 8
+    print "{0}-->{1}-->{2}".format(result.val, result.next.val, result.next.next.val)
 
     l1 = ListNode(5)
     l2 = ListNode(5)
-    s = Solution()
-    result = s.addTwoNumbers(l1, l2)
+    result = Solution().addTwoNumbers(l1, l2)
     assert result.val == 0
     assert result.next.val == 1
 
 
 
-    # print result
 
