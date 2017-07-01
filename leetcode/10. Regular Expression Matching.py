9a70851bf63716df3975f449bc9f7c3132db7e12
yjw013809 <yjw013809@gmail.com>
2017-07-01 22:33:52 +0800

    10. Regular Expression Matchin--dp

--- leetcode/10. Regular Expression Matching.py
+++ leetcode/10. Regular Expression Matching.py
@@ -5,6 +5,26 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
+        result = [[False for i in xrange(len(p)+1)] for j in xrange(len(s)+1)]
+
+        result[0][0] = True
+
+        for i in xrange(2, len(p) + 1):
+            if p[i-1] == '*':
+                result[0][i] = result[0][i-2]
+
+        for i in xrange(1, len(s)+1):
+            for j in xrange(1, len(p)+1):
+                if p[j-1] != "*":
+                    if result[i-1][j-1] is True and (s[i-1] == p[j-1] or p[j-1] == "."):
+                        result[i][j] = True
+                else:
+                    if s[i-1] != p[j-2]:
+                        result[i][j] = result[i][j-2]
+                    if s[i-1] == p[j-2] or p[j-2] == '.':
+                        result[i][j] = result[i-1][j]
+
+        return result[-1][-1]
 
 
 if __name__ == "__main__":
