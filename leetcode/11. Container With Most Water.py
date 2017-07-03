4bdf3e29b899a2f19eabd476ab814c65817455ca
raxxar1024 <yjw013809@gmail.com>
2017-07-03 15:01:21 +0800

    11. Container With Most Water

--- leetcode/11. Container With Most Water.py
+++ leetcode/11. Container With Most Water.py
@@ -4,8 +4,16 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        max_area = 0
+        for i in xrange(len(height)):
+            for j in xrange(len(height)):
+                tmp_area = min(height[i], height[j]) * abs(i-j)
+                if max_area < tmp_area:
+                    max_area = tmp_area
+        return max_area
 
 
 if __name__ == "__main__":
-    assert Solution().maxArea([1, 2, 3]) is 4
+    assert Solution().maxArea([1, 2, 3]) is 2
+    assert Solution().maxArea([1, 2, 3, 3]) is 4
 
