67dc7660dd2a84e7a814979241a265c376064ba0
raxxar1024 <yjw013809@gmail.com>
2017-07-05 19:07:17 +0800

    12. Integer to Roman----more elegant

--- leetcode/12. Integer to Roman.py
+++ leetcode/12. Integer to Roman.py
@@ -4,41 +4,17 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        i = num / 1000
-        j = num % 1000
-        tmp = "M" * i
+        roman_array = [
+            ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+            ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+            ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+            ["", "M", "MM", "MMM", "*",  "*",  "*",  "*",  "*",    "*"],
+        ]
 
-        i = j / 100
-        j %= 100
-        if 0 < i < 4:
-            tmp += "C" * i
-        elif i == 4:
-            tmp += "CD"
-        elif 4 < i < 9:
-            tmp += "D" * (i / 5) + "C" * (i % 5)
-        elif i == 9:
-            tmp += "CM"
-
-        i = j / 10
-        j %= 10
-        if 0 < i < 4:
-            tmp += "X" * i
-        elif i == 4:
-            tmp += "XL"
-        elif 4 < i < 9:
-            tmp += "L" * (i / 5) + "X" * (i % 5)
-        elif i == 9:
-            tmp += "XC"
-
-        i = j
-        if 0 < i < 4:
-            tmp += "I" * i
-        elif i == 4:
-            tmp += "IV"
-        elif 4 < i < 9:
-            tmp += "V" * (i / 5) + "I" * (i % 5)
-        elif i == 9:
-            tmp += "IX"
+        tmp = roman_array[3][(num / 1000)]
+        tmp += roman_array[2][(num % 1000 / 100)]
+        tmp += roman_array[1][(num % 100 / 10)]
+        tmp += roman_array[0][(num % 10)]
 
         return tmp
 
