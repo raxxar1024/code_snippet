a11c9359e17f5e0d55625ff2e4bced33c0b2aac5
raxxar1024 <yjw013809@gmail.com>
2017-07-06 14:55:17 +0800

    15. 3Sum

---
+++ leetcode/15. 3Sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().threeSum([-1, 0, 1, 2, -1, -4]) == [
+        [-1, 0, 1],
+        [-1, -1, 2]
+    ]
+
+
+
