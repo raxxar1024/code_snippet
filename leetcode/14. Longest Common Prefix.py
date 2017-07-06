25c4adb97a85ef3f37c1cf1cf874fe6f5ed52cc4
raxxar1024 <yjw013809@gmail.com>
2017-07-06 12:02:31 +0800

    14. Longest Common Prefix---solution

--- leetcode/14. Longest Common Prefix.py
+++ leetcode/14. Longest Common Prefix.py
@@ -4,10 +4,25 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+        if len(strs) == 0:
+            return ""
+        min_len = len(strs[0])
+        for o_str in strs:
+            min_len = min(len(o_str), min_len)
 
+        tmp = ""
+        for i in xrange(min_len):
+            for o_str in strs[1:]:
+                if o_str[i] != strs[0][i]:
+                    return tmp
+            tmp += strs[0][i]
+        return tmp
 
 if __name__ == "__main__":
     assert Solution().longestCommonPrefix([]) == ""
     assert Solution().longestCommonPrefix(["123", "1", "122"]) == "1"
     assert Solution().longestCommonPrefix(["123", "1", "q22"]) == ""
     assert Solution().longestCommonPrefix(["123"]) == "123"
+    assert Solution().longestCommonPrefix(["123", "123"]) == "123"
+
+
