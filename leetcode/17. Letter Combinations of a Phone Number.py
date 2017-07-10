1cceb688284880d87a365a89dca2ff4c5464eae5
raxxar1024 <yjw013809@gmail.com>
2017-07-10 08:46:08 +0800

    17. Letter Combinations of a Phone Number

---
+++ leetcode/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().letterCombinations("23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
+
+
