d087b9236b9a59a36f115c4ae020ba58c7b15d66
raxxar1024 <yjw013809@gmail.com>
2017-07-10 08:53:19 +0800

    17. Letter Combinations of a Phone Number---testcase

--- leetcode/17. Letter Combinations of a Phone Number.py
+++ leetcode/17. Letter Combinations of a Phone Number.py
@@ -8,6 +8,9 @@ class Solution(object):
 
 if __name__ == "__main__":
     assert Solution().letterCombinations("23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+    assert Solution().letterCombinations("10") == ["* "]
+    assert Solution().letterCombinations("12") == ["*a", "*b", "*c"]
+    assert Solution().letterCombinations("") == []
 
 
 
