1711c88fd498ab55b0a586add7d9fb9c52b1b5a7
raxxar1024 <yjw013809@gmail.com>
2017-07-12 09:52:19 +0800

    21. Merge Two Sorted Lists--solution

--- leetcode/21. Merge Two Sorted Lists.py
+++ leetcode/21. Merge Two Sorted Lists.py
@@ -11,9 +11,32 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        dummy = ListNode(-1)
+        result = dummy
+        while l1 or l2:
+            if not l1:
+                dummy.next = ListNode(l2.val)
+                l2 = l2.next
+            elif not l2:
+                dummy.next = ListNode(l1.val)
+                l1 = l1.next
+            elif l1.val > l2.val:
+                dummy.next = ListNode(l2.val)
+                l2 = l2.next
+            else:
+                dummy.next = ListNode(l1.val)
+                l1 = l1.next
+            dummy = dummy.next
+        return result.next
 
 
 if __name__ == "__main__":
+    l1 = ListNode(2)
+    l2 = ListNode(1)
+    result = Solution().mergeTwoLists(l1, l2)
+    assert result.val == 1
+    assert result.next.val == 2
+
     l1, l1.next, l1.next.next = ListNode(1), ListNode(3), ListNode(5)
     l2, l2.next, l2.next.next = ListNode(2), ListNode(4), ListNode(6)
     result = Solution().mergeTwoLists(l1, l2)
@@ -23,6 +46,8 @@ if __name__ == "__main__":
     assert result.next.next.next.val == 4
     assert result.next.next.next.next.val == 5
     assert result.next.next.next.next.next.val == 6
+
+
 
 
 
