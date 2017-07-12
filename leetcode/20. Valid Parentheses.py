0b47d851188b9d53b4927e328685464fb173e05e
raxxar1024 <yjw013809@gmail.com>
2017-07-12 09:02:07 +0800

    20. Valid Parentheses---solution

--- leetcode/20. Valid Parentheses.py
+++ leetcode/20. Valid Parentheses.py
@@ -4,6 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        tmp_stack = []
+        dict_pair = {"]": "[", "}": "{", ")": "(", }
+        lst_left = ["[", "{", "("]
+        for c in s:
+            if c in lst_left:
+                tmp_stack.append(c)
+            else:
+                if len(tmp_stack) == 0:
+                    return False
+                tmp = tmp_stack.pop()
+                if tmp != dict_pair[c]:
+                    return False
+
+        if len(tmp_stack) == 0:
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
