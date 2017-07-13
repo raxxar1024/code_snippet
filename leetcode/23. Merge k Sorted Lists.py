33e3e8735cfe5e1035dba98bd7aba7149dd97b9a
raxxar1024 <yjw013809@gmail.com>
2017-07-13 14:26:38 +0800

    23. Merge k Sorted Lists---divide and conquer

--- leetcode/23. Merge k Sorted Lists.py
+++ leetcode/23. Merge k Sorted Lists.py
@@ -11,7 +11,38 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
+        length = len(lists)
+        return self.merge_two_parts(lists[:length/2], lists[length/2:])
 
+    def merge_two_parts(self, part1, part2):
+        length1, length2 = len(part1), len(part2)
+        if length1 > 1:
+            p1 = self.merge_two_parts(part1[:length1/2], part1[length1/2:])
+        elif length1 == 1:
+            p1 = part1[0]
+        else:
+            p1 = None
+
+        if length2 > 1:
+            p2 = self.merge_two_parts(part2[:length2/2], part2[length2/2:])
+        elif length2 == 1:
+            p2 = part2[0]
+        else:
+            p2 = None
+        return self.merge_two_list(p1, p2)
+
+    def merge_two_list(self, p1, p2):
+        result = dummy = ListNode(-1)
+        while p1 and p2:
+            if p1.val > p2.val:
+                dummy.next = p2
+                p2 = p2.next
+            else:
+                dummy.next = p1
+                p1 = p1.next
+            dummy = dummy.next
+        dummy.next = p1 or p2
+        return result.next
 
 if __name__ == "__main__":
     l1 = ListNode(3)
@@ -29,4 +60,3 @@ if __name__ == "__main__":
     assert result.next.next.next.next.next.next.val == 7
     assert result.next.next.next.next.next.next.next.val == 8
 
-    
