02bacdcd6bfffd75cf6aa03e07498c3838b69341
raxxar1024 <yjw013809@gmail.com>
2017-07-13 09:36:03 +0800

    22. Generate Parentheses---solution

--- leetcode/22. Generate Parentheses.py
+++ leetcode/22. Generate Parentheses.py
@@ -4,6 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        result = []
+        def gen_string(left, right, s):
+            if left == 0 and right == 0:
+                result.append(s)
+            if left > 0:
+                gen_string(left-1, right, s+"(")
+            if right > 0 and left < right:
+                gen_string(left, right-1, s+")")
+        gen_string(n, n, "")
+        return result
 
 
 if __name__ == "__main__":
