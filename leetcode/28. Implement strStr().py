859283e866316a6d63aa30ad2a99cda71055bcce
raxxar1024 <yjw013809@gmail.com>
2017-07-17 10:21:02 +0800

    28. Implement strStr()

---
+++ leetcode/28. Implement strStr().py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+                
+
+if __name__ == "__main__":
+    assert Solution().strStr("123", "23") == 1
