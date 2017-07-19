8e433062d3051de2ff5ccd0e9730d75da8cabfaf
raxxar1024 <yjw013809@gmail.com>
2017-07-19 09:29:08 +0800

    29. Divide Two Integers

---
+++ leetcode/29. Divide Two Integers.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+
+                
+
+if __name__ == "__main__":
+    assert Solution().divide(4, 2) == 2
+    assert Solution().divide(0, 1) == 0
+    assert Solution().divide(1, 0) == -1
