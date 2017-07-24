cff639501608908ed7bdb3f1ebb75bd998f797ce
yjw013809 <yjw013809@gmail.com>
2017-07-24 23:16:59 +0800

    31. Next Permutation---solution

--- leetcode/31. Next Permutation.py
+++ leetcode/31. Next Permutation.py
@@ -4,10 +4,59 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        i = len(nums)-2
+        if i == -1:
+            return
+
+        while nums[i] >= nums[i+1]:
+            i -= 1
+            if i == -1:
+                nums.reverse()
+                return
+
+        j = len(nums)-1
+        while nums[i] >= nums[j] and j >= 0:
+            j -= 1
+
+        nums[i], nums[j] = nums[j], nums[i]
+        nums[i+1:] = nums[:i:-1]
+        return
 
 
 if __name__ == "__main__":
-    assert Solution().nextPermutation([1, 2, 3]) == [1, 3, 2]
-    assert Solution().nextPermutation([3, 2, 1]) == [1, 2, 3]
-    assert Solution().nextPermutation([1, 1, 5]) == [1, 5, 1]
+    nums = [5, 1, 1]
+    Solution().nextPermutation(nums)
+    assert nums == [1, 1, 5]
+
+    nums = [1, 1]
+    Solution().nextPermutation(nums)
+    assert nums == [1, 1]
+
+    nums = [1, 5, 1]
+    Solution().nextPermutation(nums)
+    assert nums == [5, 1, 1]
+
+    nums = [1, 3, 2]
+    Solution().nextPermutation(nums)
+    assert nums == [2, 1, 3]
+
+    nums = [1, 2]
+    Solution().nextPermutation(nums)
+    assert nums == [2, 1]
+
+    nums = [1]
+    Solution().nextPermutation(nums)
+    assert nums == [1]
+
+    nums = [1, 2, 3]
+    Solution().nextPermutation(nums)
+    assert nums == [1, 3, 2]
+
+    nums = [3, 2, 1]
+    Solution().nextPermutation(nums)
+    assert nums == [1, 2, 3]
+
+    nums = [1, 1, 5]
+    Solution().nextPermutation(nums)
+    assert nums == [1, 5, 1]
 
