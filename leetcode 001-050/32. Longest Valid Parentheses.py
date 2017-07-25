570a6813d755981f4e613e3573f80ebbb5d2ff69
raxxar1024 <yjw013809@gmail.com>
2017-07-25 15:02:53 +0800

    32. Longest Valid Parentheses---solution

--- leetcode 001-050/32. Longest Valid Parentheses.py
+++ leetcode 001-050/32. Longest Valid Parentheses.py
@@ -4,10 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        max_len, stack, last = 0, [], -1
+        for i in xrange(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    last = i
+                else:
+                    stack.pop()
+                    if len(stack) == 0:
+                        max_len = max(i-last, max_len)
+                    else:
+                        max_len = max(i-stack[-1], max_len)
+        return max_len
 
 
 if __name__ == "__main__":
-    Solution().longestValidParentheses("(()") == 2
-    Solution().longestValidParentheses(")()())") == 4
+    assert Solution().longestValidParentheses(")()())") == 4
+    assert Solution().longestValidParentheses("(()") == 2
+    assert Solution().longestValidParentheses("()(()") == 2
+    assert Solution().longestValidParentheses("()") == 2
+
 
 
