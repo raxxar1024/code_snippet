7e6dbf22c6d06ccf72aeeb0c18d41c909da3aafe
raxxar1024 <yjw013809@gmail.com>
2017-07-25 09:28:58 +0800

    31. Next Permutation---my elegant

--- leetcode/31. Next Permutation.py
+++ leetcode/31. Next Permutation.py
@@ -4,21 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        k, l = -1, 0
-        for i in xrange(len(nums)-1):
+        k = -1
+        for i in xrange(len(nums)-2, -1, -1):
             if nums[i] < nums[i+1]:
                 k = i
+                break
 
         if k == -1:
             nums.reverse()
             return
 
-        for i in xrange(k+1, len(nums)):
+        for i in xrange(len(nums)-1, k, -1):
             if nums[i] > nums[k]:
-                l = i
+                break
 
-        nums[k], nums[l] = nums[l], nums[k]
-        nums[k+1:] = nums[:k:-1]
+        nums[k], nums[i] = nums[i], nums[k]
+        nums[k+1:] = nums[k+1:][::-1]
 
 
 if __name__ == "__main__":
