f0ba8ef177070ecc4817e1c01245a1dedd36e166
yjw013809 <yjw013809@gmail.com>
2017-07-26 21:34:59 +0800

    34. Search for a Range

---
+++ leetcode 001-050/34. Search for a Range.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().searchRange([5, 7, 7, 8, 8, 10], 8) == [3, 4]
+
+
