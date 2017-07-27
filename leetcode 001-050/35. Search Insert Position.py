bd76c536e0d286f0e6784debf0b16fc95563ab50
raxxar1024 <yjw013809@gmail.com>
2017-07-27 13:56:31 +0800

    35. Search Insert Position---solution

---
+++ leetcode 001-050/35. Search Insert Position.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums)-1
+        while l <= r:
+            m = (l+r)/2
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        return l
+
+if __name__ == "__main__":
+    assert Solution().searchInsert([1, 3, 5, 6], 2) == 1
+    assert Solution().searchInsert([1, 3, 5, 6], 5) == 2
+    assert Solution().searchInsert([1, 3, 5, 6], 7) == 4
+    assert Solution().searchInsert([1, 3, 5, 6], 0) == 0
+
