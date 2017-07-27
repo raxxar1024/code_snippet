12aee10787cd475d4448a5478dd669a4c14457a4
raxxar1024 <yjw013809@gmail.com>
2017-07-27 14:05:12 +0800

    36. Valid Sudoku

---
+++ leetcode 001-050/36. Valid Sudoku.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+
+if __name__ == "__main__":
+    assert Solution().isValidSudoku(
+        [".87654321", "2........", "3........", "4........", "5........", "6........", "7........", "8........",
+         "9........"]
+    ) is True
+
+
