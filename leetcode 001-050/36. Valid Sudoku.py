37361933a5705d1bc9f110a97c8a692019b3017c
raxxar1024 <yjw013809@gmail.com>
2017-07-28 17:47:03 +0800

    36. Valid Sudoku---brute force

--- leetcode 001-050/36. Valid Sudoku.py
+++ leetcode 001-050/36. Valid Sudoku.py
@@ -4,6 +4,28 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
+        def is_valid(x, y, t):
+            for k in xrange(9):
+                if board[k][y] == t and k != x:
+                    return False
+            for k in xrange(9):
+                if board[x][k] == t and k != y:
+                    return False
+            for k in xrange(3):
+                for l in xrange(3):
+                    if board[x/3*3+k][y/3*3+l] == t and x != x/3*3+k and y != y/3*3+l:
+                        return False
+            return True
+
+        for i in xrange(9):
+            for j in xrange(9):
+                tmp = board[i][j]
+                if tmp == '.':
+                    continue
+                if is_valid(i, j, tmp) is False:
+                    return False
+        return True
+
 
 if __name__ == "__main__":
     assert Solution().isValidSudoku(
