32862e8327d3969649d860010c17cdc07eba516c
yjw013809 <yjw013809@gmail.com>
2017-07-30 22:07:26 +0800

    38. Count and Say---solution

--- leetcode 001-050/38. Count and Say.py
+++ leetcode 001-050/38. Count and Say.py
@@ -4,7 +4,22 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
+        def convert(s):
+            last, count, tmp_string = '', 0, ''
+            for j in xrange(len(s)):
+                if s[j] != last:
+                    if count != 0:
+                        tmp_string += chr(ord('0')+count) + last
+                    last, count = s[j], 1
+                else:
+                    count += 1
+            tmp_string += chr(ord('0')+count) + last
+            return tmp_string
 
+        s = "1"
+        for i in xrange(2, n+1):
+            s = convert(s)
+        return s
 
 if __name__ == "__main__":
     assert Solution().countAndSay(1) == "1"
