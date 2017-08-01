20556c9e61eab2c163fefa3eda17018a0ed8bc05
raxxar1024 <yjw013809@gmail.com>
2017-08-01 14:30:05 +0800

    40. Combination Sum II---solution

--- leetcode 001-050/40. Combination Sum II.py
+++ leetcode 001-050/40. Combination Sum II.py
@@ -1,11 +1,25 @@
 class Solution(object):
-	def combinationSum2(self, candidates, target):
-		"""
-		:type candidates: List[int]
-		:type target: int
-		:rtype: List[List[int]]
-		"""
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.combinationSum2Recu(sorted(candidates), result, 0, [], target)
+        return result
 
+    def combinationSum2Recu(self, candidates, result, start, intermediate, target):
+        if target == 0:
+            result.append(list(intermediate))
+        prev = 0
+        while start < len(candidates) and target >= candidates[start]:
+            if prev != candidates[start]:
+                intermediate.append(candidates[start])
+                self.combinationSum2Recu(candidates, result, start+1, intermediate, target-candidates[start])
+                intermediate.pop()
+                prev = candidates[start]
+            start += 1
 
 if __name__ == "__main__":
-	assert Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8) == [[1, 7], [1, 2, 5], [2, 6], [1, 1, 6]]
+    assert Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8) == [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]]
