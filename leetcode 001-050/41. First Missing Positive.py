81804409c66539e5a356445486fa8fc0080bb527
raxxar1024 <yjw013809@gmail.com>
2017-08-02 15:50:30 +0800

    41. First Missing Positive --- elegant solution

--- leetcode 001-050/41. First Missing Positive.py
+++ leetcode 001-050/41. First Missing Positive.py
@@ -16,8 +16,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        i = 0
+        while i < len(nums):
+            if len(nums) >= nums[i] > 0 and nums[i] != nums[nums[i] - 1]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+            else:
+                i += 1
+
+        for i in xrange(len(nums)):
+            if i + 1 != nums[i]:
+                return i + 1
+        return len(nums) + 1
 
 
 if __name__ == "__main__":
+    assert Solution().firstMissingPositive([1, 1]) == 2
+    assert Solution().firstMissingPositive([2]) == 1
+    assert Solution().firstMissingPositive([1]) == 2
+    assert Solution().firstMissingPositive([]) == 1
     assert Solution().firstMissingPositive([1, 2, 0]) == 3
     assert Solution().firstMissingPositive([3, 4, -1, 1]) == 2
