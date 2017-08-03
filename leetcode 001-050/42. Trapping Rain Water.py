f304cad5cfb3f1aa9542a0509f1849e83551c08d
raxxar1024 <yjw013809@gmail.com>
2017-08-03 09:11:32 +0800

    42. Trapping Rain Water---solution

--- leetcode 001-050/42. Trapping Rain Water.py
+++ leetcode 001-050/42. Trapping Rain Water.py
@@ -14,7 +14,23 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        left, result = [0] * len(height), 0
+        tmp = 0
+        for i in xrange(len(height)):
+            if tmp < height[i]:
+                tmp = height[i]
+            left[i] = tmp
+
+        tmp = 0
+        for i in xrange(len(height)-1, 0, -1):
+            if i != len(height)-1 and min(left[i-1], tmp) > height[i]:
+                result += min(left[i-1], tmp) - height[i]
+            if tmp < height[i]:
+                tmp = height[i]
+
+        return result
 
 
 if __name__ == "__main__":
+    assert Solution().trap([]) == 0
     assert Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6
