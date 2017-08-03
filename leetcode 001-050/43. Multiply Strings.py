7f57d38a297bcbc8b7caabc3029904deaeafdbe7
raxxar1024 <yjw013809@gmail.com>
2017-08-03 16:48:50 +0800

    43. Multiply Strings---solution

--- leetcode 001-050/43. Multiply Strings.py
+++ leetcode 001-050/43. Multiply Strings.py
@@ -18,7 +18,29 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
+        num1, num2 = num1[::-1], num2[::-1]
+        multiply = [0] * (len(num1) + len(num2))
+        for i in xrange(len(num1)):
+            for j in xrange(len(num2)):
+                multiply[i + j] += (ord(num1[i]) - ord("0")) * (ord(num2[j]) - ord("0"))
+
+        for i in xrange(len(multiply) - 1):
+            multiply[i + 1] += multiply[i] / 10
+            multiply[i] %= 10
+
+        i = len(multiply) - 1
+        is_start = False
+        result = ""
+        while i >= 0:
+            if multiply[i] != 0 or is_start is True:
+                is_start = True
+                result += chr(multiply[i] + ord("0"))
+            i -= 1
+
+        return result if result != "" else "0"
 
 
 if __name__ == "__main__":
+    assert Solution().multiply("0", "0") == "0"
+    assert Solution().multiply("99", "99") == "9801"
     assert Solution().multiply("11", "12") == "132"
