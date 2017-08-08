a7a2e4b32da6a111d78a80ca1e8947dc510007cf
raxxar1024 <yjw013809@gmail.com>
2017-08-08 10:18:32 +0800

    46. Permutations---solution

--- leetcode 001-050/46. Permutations.py
+++ leetcode 001-050/46. Permutations.py
@@ -21,6 +21,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        result = []
+
+        def gen_permute(l1, l2):
+            if len(l2) == 0:
+                result.append(l1)
+                return
+            else:
+                for i in xrange(len(l2)):
+                    tmp1, tmp2 = list(l1), list(l2)
+                    tmp1.append(tmp2[i])
+                    del tmp2[i]
+                    gen_permute(tmp1, tmp2)
+
+        gen_permute([], nums)
+        return result
 
 
 if __name__ == "__main__":
