37ae15d6276dac71e8d6d6d969ba2f908c22668e
raxxar1024 <yjw013809@gmail.com>
2017-08-09 10:23:00 +0800

    49. Group Anagrams---solution but TLE

--- leetcode 001-050/49. Group Anagrams.py
+++ leetcode 001-050/49. Group Anagrams.py
@@ -20,7 +20,22 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
+        from collections import OrderedDict
+        results = OrderedDict()
+        result = []
+        for o_str in strs:
+            tmp_list = list(o_str)
+            tmp_list.sort()
+            tmp = str(tmp_list)
+            if tmp in results.keys():
+                results[tmp].append(o_str)
+            else:
+                results[tmp] = [o_str]
 
+        for k, v in results.items():
+            result.append(v)
+
+        return result
 
 if __name__ == "__main__":
     assert Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) == [
