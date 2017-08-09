682322cd359cd8e1447522b3ceec9de3f00f7ac7
raxxar1024 <yjw013809@gmail.com>
2017-08-09 10:04:19 +0800

    48. Rotate Image---solution

--- leetcode 001-050/48. Rotate Image.py
+++ leetcode 001-050/48. Rotate Image.py
@@ -15,7 +15,14 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
         """
+        matrix.reverse()
+        for i in xrange(len(matrix)):
+            for j in xrange(len(matrix[0])):
+                if i > j:
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
 
 if __name__ == "__main__":
-    assert Solution().rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    Solution().rotate(matrix)
+    assert matrix == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
