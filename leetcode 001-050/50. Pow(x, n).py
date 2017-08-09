bf8567f447816f2c3b1b35367fd27ea2cf2d5b5d
yjw013809 <yjw013809@gmail.com>
2017-08-09 23:16:37 +0800

    50. Pow(x, n)

---
+++ leetcode 001-050/50. Pow(x, n).py
@@ -0,0 +1,16 @@
+"""
+Implement pow(x, n).
+"""
+
+
+class Solution(object):
+	def myPow(self, x, n):
+		"""
+		:type x: float
+		:type n: int
+		:rtype: float
+		"""
+        
+
+if __name__ == "__main__":
+	assert Solution().myPow(8.88023, 3) == 700.28148
