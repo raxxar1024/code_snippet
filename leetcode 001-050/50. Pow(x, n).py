c70b71f7a965f8d761c03dc08cc127bea64547e8
raxxar1024 <yjw013809@gmail.com>
2017-08-10 09:34:27 +0800

    50. Pow(x, n)---TLE

--- leetcode 001-050/50. Pow(x, n).py
+++ leetcode 001-050/50. Pow(x, n).py
@@ -1,16 +1,27 @@
 """
 Implement pow(x, n).
+
 """
 
 
 class Solution(object):
-	def myPow(self, x, n):
-		"""
-		:type x: float
-		:type n: int
-		:rtype: float
-		"""
-        
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        result = 1
+        if n > 0:
+            while n > 0:
+                result *= x
+                n -= 1
+        else:
+            while n < 0:
+                result /= x
+                n += 1
+        return result
+
 
 if __name__ == "__main__":
-	assert Solution().myPow(8.88023, 3) == 700.28148
+    assert Solution().myPow(8.88023, 3) == 700.28148
