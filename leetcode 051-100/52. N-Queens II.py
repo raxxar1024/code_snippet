28c4856906cca6a265f8d09e28cfc525bf604335
yjw013809 <yjw013809@gmail.com>
2017-08-12 22:44:54 +0800

    52. N-Queens II

---
+++ leetcode 051-100/52. N-Queens II.py
@@ -0,0 +1,18 @@
+"""
+Follow up for N-Queens problem.
+
+Now, instead outputting board configurations, return the total number of distinct solutions.
+
+"""
+
+
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().totalNQueens(1) == 1
