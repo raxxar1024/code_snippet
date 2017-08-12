4f5cbbcb94cec7269ba7881af63507e4ac5cd86c
yjw013809 <yjw013809@gmail.com>
2017-08-12 22:48:45 +0800

    52. N-Queens II---DFS, follow last question.

--- leetcode 051-100/52. N-Queens II.py
+++ leetcode 051-100/52. N-Queens II.py
@@ -12,6 +12,21 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        positions = []
+
+        def DFS(ys, xy_diff, xy_sum):
+            if n == len(ys):
+                positions.append(ys)
+                return
+            else:
+                x = len(ys)
+                for y in xrange(n):
+                    if y not in ys and (x - y) not in xy_diff and (x + y) not in xy_sum:
+                        DFS(ys + [y], xy_diff + [x - y], xy_sum + [x + y])
+
+        DFS([], [], [])
+
+        return len(positions)
 
 
 if __name__ == "__main__":
