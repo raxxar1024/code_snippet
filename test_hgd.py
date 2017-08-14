1caf5993ea5a42fb264ec7291ae0ca9423f12345
raxxar1024 <yjw013809@gmail.com>
2017-08-14 17:31:01 +0800

    完善进入测试界面的过程

--- test_hgd.py
+++ test_hgd.py
@@ -4,4 +4,22 @@ from splinter.browser import Browser
 
 if __name__ == '__main__':
     b = Browser()
-    b.visit("http://www.baidu.com/")
+    b.visit("http://192.168.1.131:8002")
+
+    # login in
+    b.fill('name', u'张三')
+    b.fill('code', '123')
+    b.fill('pwd', '1')
+    b.find_by_name('submit').first.click()
+
+    # choose
+    b.choose('classroom_id', '2')
+    b.choose('course_id', '2')
+    b.find_by_name('submit').first.click()
+
+    # enter discuss-detail
+    b.visit('http://192.168.1.131:8002/discuss-detail_19')
+
+    while True:
+        import time
+        time.sleep(5)
