48f5eb647a0e083789179e210ce4d52be38b4482
raxxar1024 <yjw013809@gmail.com>
2017-08-15 09:47:42 +0800

    55. Jump Game---solution

--- leetcode 051-100/55. Jump Game.py
+++ leetcode 051-100/55. Jump Game.py
@@ -19,8 +19,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        last = 0
+        for i in xrange(len(nums)):
+            if i > last:
+                break
+            last = max(last, i + nums[i])
+
+        return last >= len(nums) - 1
 
 
 if __name__ == "__main__":
+    assert Solution().canJump([5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]) is True
+    assert Solution().canJump([3, 2, 1]) is True
+    assert Solution().canJump([2, 1, 2, 2, 1, 2, 2, 2]) is True
+    assert Solution().canJump([1, 0, 1, 0]) is False
+    assert Solution().canJump([0, 2, 3]) is False
     assert Solution().canJump([2, 3, 1, 1, 4]) is True
     assert Solution().canJump([3, 2, 1, 0, 4]) is False
