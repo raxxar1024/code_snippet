8a50d419922083702b9b496ba572f8e5cdfb8de9
raxxar1024 <yjw013809@gmail.com>
2017-08-15 09:56:48 +0800

    56. Merge Intervals

---
+++ leetcode 051-100/56. Merge Intervals.py
@@ -0,0 +1,30 @@
+"""
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18].
+
+"""
+
+
+# Definition for an interval.
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+    def __repr__(self):
+        return "[{}--{}]".format(self.start, self.end)
+
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+
+
+if __name__ == "__main__":
+    print Solution().merge([Interval(1, 3), Interval(2, 6), Interval(8, 10), Interval(15, 18)])
