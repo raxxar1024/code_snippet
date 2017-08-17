6250d6f10d4c5f875b6a310c97585db782dabc4f
raxxar1024 <yjw013809@gmail.com>
2017-08-17 11:20:57 +0800

    59. Spiral Matrix II---solution

--- leetcode 051-100/59. Spiral Matrix II.py
+++ leetcode 051-100/59. Spiral Matrix II.py
@@ -20,6 +20,29 @@ class Solution(object):
         :type n: int
         :rtype: List[List[int]]
         """
+        top, bottom, left, right = 0, n - 1, 0, n - 1
+        result = [[0 for i in xrange(n)] for i in xrange(n)]
+        i = 0
+        while i < n * n:
+            for k in xrange(left, right + 1):
+                result[top][k] = i + 1
+                i += 1
+            top += 1
+            for k in xrange(top, bottom + 1):
+                result[k][right] = i + 1
+                i += 1
+            right -= 1
+            if i < n * n:
+                for k in xrange(right, left - 1, -1):
+                    result[bottom][k] = i + 1
+                    i += 1
+                bottom -= 1
+            if i < n * n:
+                for k in xrange(bottom, top - 1, -1):
+                    result[k][left] = i + 1
+                    i += 1
+                left += 1
+        return result
 
 
 if __name__ == "__main__":
