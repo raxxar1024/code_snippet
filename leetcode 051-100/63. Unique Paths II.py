ca520a513a08f60ae9b386731797d0dd951c3665
yjw013809 <yjw013809@gmail.com>
2017-08-19 23:17:35 +0800

    63. Unique Paths II---solution

--- leetcode 051-100/63. Unique Paths II.py
+++ leetcode 051-100/63. Unique Paths II.py
@@ -26,7 +26,40 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        result = [[1 for _ in xrange(n)] for _ in xrange(m)]
+
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        for i in xrange(1, n):
+            if obstacleGrid[0][i] == 1:
+                result[0][i] = 0
+            else:
+                result[0][i] = result[0][i - 1]
+
+        for i in xrange(1, m):
+            if obstacleGrid[i][0] == 1:
+                result[i][0] = 0
+            else:
+                result[i][0] = result[i - 1][0]
+
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                result[i][j] = 0
+                if obstacleGrid[i][j] == 1:
+                    continue
+                if obstacleGrid[i - 1][j] == 0:
+                    result[i][j] += result[i - 1][j]
+                if obstacleGrid[i][j - 1] == 0:
+                    result[i][j] += result[i][j - 1]
+        return result[m - 1][n - 1]
 
 
 if __name__ == "__main__":
+    assert Solution().uniquePathsWithObstacles([[0, 0], [0, 1]]) == 0
+    assert Solution().uniquePathsWithObstacles([[0, 0]]) == 1
+    assert Solution().uniquePathsWithObstacles([[0, 1]]) == 0
+    assert Solution().uniquePathsWithObstacles([[1]]) == 0
     assert Solution().uniquePathsWithObstacles([[0, 0, 0], [0, 1, 0], [0, 0, 0]]) == 2
