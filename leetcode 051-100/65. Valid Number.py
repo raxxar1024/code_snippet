8eae17db5701166b41de2c46fef045734e52af7e
yjw013809 <yjw013809@gmail.com>
2017-08-20 13:30:35 +0800

    65. Valid Number

---
+++ leetcode 051-100/65. Valid Number.py
@@ -0,0 +1,29 @@
+"""
+Validate if a given string is numeric.
+
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+Note: It is intended for the problem statement to be ambiguous.
+You should gather all requirements up front before implementing one.
+
+"""
+
+
+class Solution(object):
+    def isNumber(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().isNumber("0") is True
+    assert Solution().isNumber("0.1") is True
+    assert Solution().isNumber("abc") is False
+    assert Solution().isNumber("1 a") is False
+    assert Solution().isNumber("2e10") is True
