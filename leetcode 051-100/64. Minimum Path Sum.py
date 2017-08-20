cb28a0db0baa3e8e8c90acf90e185fff31151303
yjw013809 <yjw013809@gmail.com>
2017-08-20 13:27:11 +0800

    64. Minimum Path Sum---solution II

--- leetcode 051-100/64. Minimum Path Sum.py
+++ leetcode 051-100/64. Minimum Path Sum.py
@@ -7,7 +7,7 @@ Note: You can only move either down or right at any point in time.
 
 
 class Solution(object):
-    def minPathSum(self, grid):
+    def minPathSum2(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
@@ -26,7 +26,26 @@ class Solution(object):
 
         return result[m - 1][n - 1]
 
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        n = len(grid[0])
+        result = [0] * n
+        result[0] = grid[0][0]
+
+        for i in xrange(1, n):
+            result[i] = result[i-1]+grid[0][i]
+
+        for i in xrange(1, len(grid)):
+            result[0] += grid[i][0]
+            for j in xrange(1, n):
+                result[j] = min(result[j-1], result[j]) + grid[i][j]
+
+        return result[-1]
+
 
 if __name__ == "__main__":
-    # assert Solution().minPathSum([[1, 2], [1, 1]]) == 3
+    assert Solution().minPathSum([[1, 2], [1, 1]]) == 3
     assert Solution().minPathSum([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == 21
