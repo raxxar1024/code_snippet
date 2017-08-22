ff746bf6eace838f7b7738ed8c29bc4493f3fc34
yjw013809 <yjw013809@gmail.com>
2017-08-22 22:30:32 +0800

    67. Add Binary---solution

--- leetcode 051-100/67. Add Binary.py
+++ leetcode 051-100/67. Add Binary.py
@@ -16,6 +16,28 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
+        len_a, len_b = len(a), len(b)
+        if len_a > len_b:
+            b = "0" * (len_a - len_b) + b
+        else:
+            a = "0" * (len_b - len_a) + a
+
+        carry = 0
+        tmp = [0] * max(len_a, len_b)
+        for i in reversed(xrange(max(len_a, len_b))):
+            tmp[i] = int(a[i]) + int(b[i]) + carry
+            carry = tmp[i] / 2
+            tmp[i] %= 2
+        if carry == 1:
+            tmp = [1] + tmp
+
+        result = ""
+        for i in xrange(len(tmp)):
+            result += str(tmp[i])
+
+        return result
+
+
 
 
 if __name__ == "__main__":
