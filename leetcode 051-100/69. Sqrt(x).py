758e6e599a7388082d4a9a5fcd35148bf96a6529
yjw013809 <yjw013809@gmail.com>
2017-08-23 22:04:49 +0800

    69. Sqrt(x)

---
+++ leetcode 051-100/69. Sqrt(x).py
@@ -0,0 +1,19 @@
+"""
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+"""
+
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().mySqrt(9) == 3
+    assert Solution().mySqrt(8) == 2
