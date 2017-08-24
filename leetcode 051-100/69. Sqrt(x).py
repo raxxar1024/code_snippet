1fbfb7d155edde7dce9845bde4fb38f65872b6ee
raxxar1024 <yjw013809@gmail.com>
2017-08-24 09:07:50 +0800

    69. Sqrt(x)---solution

--- leetcode 051-100/69. Sqrt(x).py
+++ leetcode 051-100/69. Sqrt(x).py
@@ -12,8 +12,21 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+        left, right = 0, x
+        while left <= right:
+            mid = (left + right) / 2
+            tmp = mid * mid
+            if tmp > x:
+                right = mid - 1
+            elif tmp < x:
+                left = mid + 1
+            else:
+                return mid
+        return left - 1
 
 
 if __name__ == "__main__":
+    assert Solution().mySqrt(1) == 1
+    assert Solution().mySqrt(0) == 0
     assert Solution().mySqrt(9) == 3
     assert Solution().mySqrt(8) == 2
