f2c26b13c8189f47b88614595f826a93c8d3a480
raxxar1024 <yjw013809@gmail.com>
2017-08-29 09:06:29 +0800

    71. Simplify Path---solution

--- leetcode 051-100/71. Simplify Path.py
+++ leetcode 051-100/71. Simplify Path.py
@@ -21,9 +21,32 @@ class Solution(object):
         :type path: str
         :rtype: str
         """
+        words = path.split("/")
+        result_words_tmp = []
+
+        for i in xrange(len(words)):
+            if words[i] == "." or words[i] == "":
+                continue
+            else:
+                result_words_tmp.append(words[i])
+
+        if len(result_words_tmp) == 0:
+            return "/"
+
+        result_words = []
+        for i in xrange(len(result_words_tmp)):
+            if result_words_tmp[i] == "..":
+                if len(result_words) != 0:
+                    result_words.pop()
+            else:
+                result_words.append(result_words_tmp[i])
+
+        return "/" + "/".join(result_words)
 
 
 if __name__ == "__main__":
+    assert Solution().simplifyPath("/home/../../..") == "/"
+    assert Solution().simplifyPath("/") == "/"
     assert Solution().simplifyPath("/home/") == "/home"
     assert Solution().simplifyPath("/a/./b/../../c/") == "/c"
     assert Solution().simplifyPath("/../") == "/"
