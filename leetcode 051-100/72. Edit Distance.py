2a542d59e63f28d31af3da3f426ca57eb2d8986f
raxxar1024 <yjw013809@gmail.com>
2017-08-30 09:54:34 +0800

    72. Edit Distance---solution

--- leetcode 051-100/72. Edit Distance.py
+++ leetcode 051-100/72. Edit Distance.py
@@ -18,6 +18,21 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
+        m, n = len(word1), len(word2)
+        distance = [[0 for _ in xrange(n + 1)] for _ in xrange(m + 1)]
+        for i in xrange(m + 1):
+            distance[i][0] = i
+        for i in xrange(n + 1):
+            distance[0][i] = i
+        for i in xrange(1, m + 1):
+            for j in xrange(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    tmp = distance[i - 1][j - 1]
+                else:
+                    tmp = distance[i - 1][j - 1] + 1
+                distance[i][j] = min(tmp, min(distance[i - 1][j] + 1, distance[i][j - 1] + 1))
+
+        return distance[-1][-1]
 
 
 if __name__ == "__main__":
