858b1c8eb728a89de0c6de084f4bb0cd1a4a653b
raxxar1024 <yjw013809@gmail.com>
2017-08-31 10:07:42 +0800

    74. Search a 2D Matrix---solution

--- leetcode 051-100/74. Search a 2D Matrix.py
+++ leetcode 051-100/74. Search a 2D Matrix.py
@@ -25,11 +25,39 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
+        if len(matrix) == 0:
+            return False
+        if len(matrix[0]) == 0:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        begin, end = 0, m - 1
+        while begin <= end:
+            middle = (begin + end) / 2
+            if matrix[middle][0] < target:
+                begin = middle + 1
+            elif matrix[middle][0] > target:
+                end = middle - 1
+            else:
+                return True
+
+        row = begin - 1
+
+        begin, end = 0, n - 1
+        while begin <= end:
+            middle = (begin + end) / 2
+            if matrix[row][middle] < target:
+                begin = middle + 1
+            elif matrix[row][middle] > target:
+                end = middle - 1
+            else:
+                return True
+
+        return False
 
 
 if __name__ == "__main__":
-    assert Solution().searchMatrix([
-        [1, 3, 5, 7],
-        [10, 11, 16, 20],
-        [23, 30, 34, 50]
-    ], 3) is True
+    assert Solution().searchMatrix([[]], 1) is False
+    assert Solution().searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50]], 11) is True
+    assert Solution().searchMatrix([[1]], 1) is True
+    assert Solution().searchMatrix([], 0) is False
+    assert Solution().searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50]], 3) is True
