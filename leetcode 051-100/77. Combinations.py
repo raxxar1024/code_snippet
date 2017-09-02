eddfe0190f762366c1c0516f4c907bec7cab0b73
yjw013809 <yjw013809@gmail.com>
2017-09-02 23:13:14 +0800

    77. Combinations---solution but TLE

--- leetcode 051-100/77. Combinations.py
+++ leetcode 051-100/77. Combinations.py
@@ -22,7 +22,20 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        result = []
+        self.combine_rec(n, result, 0, [], k)
+        return result
+
+    def combine_rec(self, n, result, start, intermediate, k):
+        if k == 0:
+            result.append(intermediate[:])
+        else:
+            for i in xrange(start, n):
+                intermediate.append(i + 1)
+                self.combine_rec(n, result, i + 1, intermediate, k - 1)
+                intermediate.pop()
 
 
 if __name__ == "__main__":
+    print Solution().combine(20, 16)
     assert Solution().combine(4, 2) == [[2, 4], [3, 4], [2, 3], [1, 2], [1, 3], [1, 4], ]
