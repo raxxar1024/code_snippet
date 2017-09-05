d30fa270f6327d71627c7fe2ae13ff235c35ad18
raxxar1024 <yjw013809@gmail.com>
2017-09-05 09:54:24 +0800

    76. Minimum Window Substring---solution

--- leetcode 051-100/76. Minimum Window Substring.py
+++ leetcode 051-100/76. Minimum Window Substring.py
@@ -21,7 +21,37 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
+        start, end, min_start, min_end = 0, 0, 0, len(s)+1
+        dict_t_standard, dict_t = {}, {}
+        for word in t:
+            if word in dict_t_standard:
+                dict_t_standard[word] += 1
+            else:
+                dict_t_standard[word] = 1
+                dict_t[word] = 0
+
+        def check_dict(d1, d2):
+            for k in d1:
+                if d1[k] > d2[k]:
+                    return False
+            return True
+
+        for i in xrange(len(s)):
+            if s[i] in dict_t:
+                dict_t[s[i]] += 1
+            while check_dict(dict_t_standard, dict_t):
+                if i - start < min_end - min_start:
+                    min_start, min_end = start, i
+                if s[start] in dict_t:
+                    dict_t[s[start]] -= 1
+                start += 1
+
+        if (min_end - min_start) > len(s):
+            return ""
+        else:
+            return s[min_start:min_end + 1]
 
 
 if __name__ == "__main__":
+    assert Solution().minWindow("a", "aa") == ""
     assert Solution().minWindow("ADOBECODEBANC", "ABC") == "BANC"
