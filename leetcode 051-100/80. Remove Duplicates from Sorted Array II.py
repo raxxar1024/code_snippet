b56f87d668900f10f31c31055fccdff12a1f4498
raxxar1024 <yjw013809@gmail.com>
2017-09-06 10:05:26 +0800

    80. Remove Duplicates from Sorted Array II---solution

--- leetcode 051-100/80. Remove Duplicates from Sorted Array II.py
+++ leetcode 051-100/80. Remove Duplicates from Sorted Array II.py
@@ -17,6 +17,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        dict_nums, delete_list = {}, []
+        
+        for i in xrange(len(nums)):
+            if nums[i] in dict_nums:
+                if dict_nums[nums[i]] >= 2:
+                    delete_list.append(i)
+                else:
+                    dict_nums[nums[i]] += 1
+            else:
+                dict_nums[nums[i]] = 1
+
+        for i in reversed(delete_list):
+            del nums[i]
+
+        return len(nums)
 
 
 if __name__ == "__main__":
