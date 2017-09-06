236de1c213258121be23125847cc26b523f89c68
raxxar1024 <yjw013809@gmail.com>
2017-09-06 15:02:13 +0800

    81. Search in Rotated Sorted Array II---solution

--- leetcode 051-100/81. Search in Rotated Sorted Array II.py
+++ leetcode 051-100/81. Search in Rotated Sorted Array II.py
@@ -21,7 +21,29 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            middle = (start + end) / 2
+            if nums[middle] == target:
+                return True
+            elif nums[middle] == nums[end]:
+                end -= 1
+            else:
+                if nums[middle] < nums[end]:
+                    if nums[middle] < target <= nums[end]:
+                        start = middle + 1
+                    else:
+                        end = middle - 1
+                else:
+                    if nums[start] <= target < nums[middle]:
+                        end = middle - 1
+                    else:
+                        start = middle + 1
+        return False
 
 
 if __name__ == "__main__":
+    assert Solution().search([3, 1, 1], 3) is True
+    assert Solution().search([1, 3], 3) is True
+    assert Solution().search([1], 1) is True
     assert Solution().search([4, 5, 6, 7, 0, 1, 2], 5) is True
