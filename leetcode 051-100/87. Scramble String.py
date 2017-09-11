f62b31264fa25e30c60f9c4deeace7636fc8887b
yjw013809 <yjw013809@gmail.com>
2017-09-11 22:43:50 +0800

    87. Scramble String---solution

--- leetcode 051-100/87. Scramble String.py
+++ leetcode 051-100/87. Scramble String.py
@@ -46,6 +46,19 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
+        if s1 == s2:
+            return True
+        if len(s1) != len(s2):
+            return False
+        if sorted(s1) != sorted(s2):
+            return False
+        length = len(s1)
+        for i in xrange(1, length):
+            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
+                return True
+            if self.isScramble(s1[:i], s2[length - i:]) and self.isScramble(s1[i:], s2[:length - i]):
+                return True
+        return False
 
 
 if __name__ == "__main__":
