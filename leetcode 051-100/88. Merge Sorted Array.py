f7e18ad140cab20739ef9e90979c27aad76d42c3
raxxar1024 <yjw013809@gmail.com>
2017-09-12 11:08:16 +0800

    88. Merge Sorted Array---solution

--- leetcode 051-100/88. Merge Sorted Array.py
+++ leetcode 051-100/88. Merge Sorted Array.py
@@ -17,9 +17,33 @@ class Solution(object):
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
+        i, j, k = m - 1, n - 1, m + n - 1
+        while i >= 0 or j >= 0:
+            if i < 0:
+                nums1[k] = nums2[j]
+                j -= 1
+                k -= 1
+                continue
+            if j < 0:
+                break
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
 
 
 if __name__ == "__main__":
+    num1, m, num2, n = [0], 0, [1], 1
+    Solution().merge(num1, m, num2, n)
+    assert num1 == [1]
+
+    num1, m, num2, n = [1, 3, 5, 0, 0, 0, 0], 3, [2, 4, 6], 3
+    Solution().merge(num1, m, num2, n)
+    assert num1 == [1, 2, 3, 4, 5, 6, 0]
+
     num1, m, num2, n = [1], 1, [], 0
     Solution().merge(num1, m, num2, n)
     assert num1 == [1]
