1c568d3137bd8d0fb33515ef7b62f671f32d124a
raxxar1024 <yjw013809@gmail.com>
2017-09-13 17:12:23 +0800

    90. Subsets II---solution

--- leetcode 051-100/90. Subsets II.py
+++ leetcode 051-100/90. Subsets II.py
@@ -24,6 +24,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        result = []
+        nums.sort()
+
+        def merge(part_1, lst_part_2):
+            if part_1 not in result:
+                result.append(part_1)
+            if not lst_part_2:
+                return
+            for i in xrange(len(lst_part_2)):
+                tmp = part_1 + [lst_part_2[i]]
+                merge(tmp, lst_part_2[i + 1:])
+
+        merge([], nums)
+        return result
 
 
 if __name__ == "__main__":
