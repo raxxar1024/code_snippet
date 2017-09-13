2be69315a488c00fd4c99a85a7491d53473ebdf7
raxxar1024 <yjw013809@gmail.com>
2017-09-13 09:09:32 +0800

    90. Subsets II

---
+++ leetcode 051-100/90. Subsets II.py
@@ -0,0 +1,37 @@
+"""
+Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+
+Note: The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+
+"""
+
+
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().subsetsWithDup([1, 2, 2]) == [
+        [2],
+        [1],
+        [1, 2, 2],
+        [2, 2],
+        [1, 2],
+        []
+    ]
