f5d9ca0da40b7f99517efc4ba3ad13bc8c3415f3
raxxar1024 <yjw013809@gmail.com>
2017-09-18 14:49:07 +0800

    96. Unique Binary Search Trees---solution

--- leetcode 051-100/96. Unique Binary Search Trees.py
+++ leetcode 051-100/96. Unique Binary Search Trees.py
@@ -19,7 +19,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        counts = [1, 1]
+        for i in xrange(2, n + 1):
+            count = 0
+            for j in xrange(i):
+                count += counts[j] * counts[i - j - 1]
+            counts.append(count)
+        return counts[-1]
 
 
 if __name__ == "__main__":
-    print Solution().numTrees(3) == 5
+    assert Solution().numTrees(3) == 5
