096f1984a1d91f7cc42e68f35621dcd1045921d1
raxxar1024 <yjw013809@gmail.com>
2017-09-18 14:52:23 +0800

    97. Interleaving String

---
+++ leetcode 051-100/97. Interleaving String.py
@@ -0,0 +1,27 @@
+"""
+Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+For example,
+Given:
+s1 = "aabcc",
+s2 = "dbbca",
+
+When s3 = "aadbbcbcac", return true.
+When s3 = "aadbbbaccc", return false.
+
+"""
+
+
+class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().isInterleave("aabcc", "dbbca", "aadbbcbcac") is True
+    assert Solution().isInterleave("aabcc", "dbbca", "aadbbbaccc") is True
