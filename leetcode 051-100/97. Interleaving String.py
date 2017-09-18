afc4b16d0c2580f66cf9b9077dc34367afc4b560
yjw013809 <yjw013809@gmail.com>
2017-09-18 22:12:43 +0800

    PEP8

--- leetcode 051-100/97. Interleaving String.py
+++ leetcode 051-100/97. Interleaving String.py
@@ -25,18 +25,16 @@ class Solution(object):
         match = [[False for _ in xrange(len(s1) + 1)] for _ in xrange(len(s2) + 1)]
         match[0][0] = True
         for i in xrange(1, len(s1) + 1):
-            match[0][i] = match[0][i-1] and s1[i-1] == s3[i-1]
-        for i in xrange(1, len(s2)+1):
-            match[i][0] = match[i-1][0] and s2[i-1] == s3[i-1]
+            match[0][i] = match[0][i - 1] and s1[i - 1] == s3[i - 1]
+        for i in xrange(1, len(s2) + 1):
+            match[i][0] = match[i - 1][0] and s2[i - 1] == s3[i - 1]
 
         for i in xrange(1, len(s1) + 1):
-            for j in xrange(1, len(s2)+1):
-                match[j][i] = (match[j][i-1] and s1[i-1] == s3[i+j-1]) or (match[j-1][i] and s2[j-1] == s3[i+j-1])
+            for j in xrange(1, len(s2) + 1):
+                match[j][i] = (match[j][i - 1] and s1[i - 1] == s3[i + j - 1]) or (
+                match[j - 1][i] and s2[j - 1] == s3[i + j - 1])
 
         return match[-1][-1]
-
-
-
 
 
 if __name__ == "__main__":
