274e4507f9cb90933f6d2d47db256df4c0170bae
yjw013809 <yjw013809@gmail.com>
2017-09-22 22:01:44 +0800

    102. Binary Tree Level Order Traversal---solution

--- leetcode 101-150/102. Binary Tree Level Order Traversal.py
+++ leetcode 101-150/102. Binary Tree Level Order Traversal.py
@@ -32,6 +32,20 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        if root is None:
+            return []
+        result, current = [], [root]
+        while current:
+            next_level, vals = [], []
+            for node in current:
+                vals.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            current = next_level
+            result.append(vals)
+        return result
 
 
 if __name__ == "__main__":
