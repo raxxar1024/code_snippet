f577f71842294603f78853a0f76ad37c22c3fa7d
yjw013809 <yjw013809@gmail.com>
2017-09-25 22:01:01 +0800

    112. Path Sum---solution

--- leetcode 101-150/112. Path Sum.py
+++ leetcode 101-150/112. Path Sum.py
@@ -31,6 +31,12 @@ class Solution(object):
         :type sum: int
         :rtype: bool
         """
+        if root is None:
+            return False
+        if root.left is None and root.right is None and root.val == sum:
+            return True
+        else:
+            return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
 
 
 if __name__ == "__main__":
