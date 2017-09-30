d396631bfaa51005fe862d6be3fe91325c323500
raxxar1024 <yjw013809@gmail.com>
2017-09-30 10:06:22 +0800

    120. Triangle---solution

--- leetcode 101-150/120. Triangle.py
+++ leetcode 101-150/120. Triangle.py
@@ -22,6 +22,16 @@ class Solution(object):
         :type triangle: List[List[int]]
         :rtype: int
         """
+        if triangle is None:
+            return 0
+        current_level = triangle[0] + [float("inf")]
+        for i in xrange(1, len(triangle)):
+            next_level = [current_level[0] + triangle[i][0]]
+            for j in xrange(1, len(triangle[i])):
+                next_level.append(min(current_level[j - 1], current_level[j]) + triangle[i][j])
+            next_level.append(float("inf"))
+            current_level = next_level
+        return reduce(min, current_level)
 
 
 if __name__ == "__main__":
