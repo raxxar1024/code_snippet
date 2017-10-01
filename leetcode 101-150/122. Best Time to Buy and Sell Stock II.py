f3c29b049b9595789aa0d466b38efc7664e67137
yjw013809 <yjw013809@gmail.com>
2017-10-01 21:26:13 +0800

    122. Best Time to Buy and Sell Stock II

---
+++ leetcode 101-150/122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,21 @@
+"""
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit.
+You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times).
+However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+"""
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().maxProfit([7, 1, 5, 3, 6, 4]) == 7
+    assert Solution().maxProfit([7, 6, 4, 3, 1]) == 0
