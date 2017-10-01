a8a3696b25608592e2422b27b5336a143dc83d5e
yjw013809 <yjw013809@gmail.com>
2017-10-01 22:03:47 +0800

    122. Best Time to Buy and Sell Stock II---solution

--- leetcode 101-150/122. Best Time to Buy and Sell Stock II.py
+++ leetcode 101-150/122. Best Time to Buy and Sell Stock II.py
@@ -14,6 +14,11 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        amount_profit = 0
+        for i in xrange(len(prices) - 1):
+            if prices[i + 1] - prices[i] > 0:
+                amount_profit += prices[i + 1] - prices[i]
+        return amount_profit
 
 
 if __name__ == "__main__":
