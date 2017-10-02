3321a9cdc8220d2bf7578b3108630f3b698f6bba
yjw013809 <yjw013809@gmail.com>
2017-10-02 23:32:23 +0800

    123. Best Time to Buy and Sell Stock III---solution

--- leetcode 101-150/123. Best Time to Buy and Sell Stock III.py
+++ leetcode 101-150/123. Best Time to Buy and Sell Stock III.py
@@ -15,8 +15,16 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        hold_1, release_1, hold_2, release_2 = float("-inf"), 0, float("-inf"), 0
+        for price in prices:
+            release_2 = max(release_2, hold_2 + price)
+            hold_2 = max(hold_2, release_1 - price)
+            release_1 = max(release_1, hold_1 + price)
+            hold_1 = max(hold_1, 0 - price)
+        return release_2
 
 
 if __name__ == "__main__":
-    assert Solution().maxProfit([7, 1, 5, 3, 6, 4]) == 7
+    # assert Solution().maxProfit([7, 1, 5, 3, 6, 4]) == 7
+    assert Solution().maxProfit([3, 1, 2, 8, 3, 1, 9, 6]) == 15
     assert Solution().maxProfit([7, 6, 4, 3, 1]) == 0
