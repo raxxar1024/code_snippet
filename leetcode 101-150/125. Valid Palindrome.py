b4fa769d742c54ffd23db0e7fdd94b79e495974b
yjw013809 <yjw013809@gmail.com>
2017-10-08 22:08:14 +0800

    125. Valid Palindrome---solution

--- leetcode 101-150/125. Valid Palindrome.py
+++ leetcode 101-150/125. Valid Palindrome.py
@@ -20,8 +20,31 @@ class Solution(object):
         :rtype: bool
         """
 
+        def is_char(c):
+            if ord('a') <= ord(c) <= ord('z') or ord('A') <= ord(c) <= ord('Z') or ord('0') <= ord(c) <= ord('9'):
+                return True
+            else:
+                return False
+
+        left, right = 0, len(s) - 1
+        while left < right:
+            if not is_char(s[left]):
+                left += 1
+                continue
+            if not is_char(s[right]):
+                right -= 1
+                continue
+            if s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
+
 
 if __name__ == "__main__":
+    assert Solution().isPalindrome(",P") is True
+    assert Solution().isPalindrome("0P") is False
     assert Solution().isPalindrome("A man, a plan, a canal: Panama") is True
     assert Solution().isPalindrome("race a car") is False
     assert Solution().isPalindrome("") is True
