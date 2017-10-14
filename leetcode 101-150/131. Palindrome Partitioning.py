29c5c3fcda58d58234cec4610a53eb01cc2038b5
raxxar1024 <yjw013809@gmail.com>
2017-10-14 10:12:29 +0800

    131. Palindrome Partitioning---solution

--- leetcode 101-150/131. Palindrome Partitioning.py
+++ leetcode 101-150/131. Palindrome Partitioning.py
@@ -20,10 +20,22 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
+        result = []
+
+        def get_partition(tmp_path, tmp_s):
+            if not tmp_s:
+                result.append(tmp_path)
+                return
+            for i in xrange(len(tmp_s)):
+                if self.is_valid(tmp_s[:i + 1]):
+                    get_partition(tmp_path + [tmp_s[:i + 1]], tmp_s[i + 1:])
+
+        get_partition([], s)
+        return result
+
+    def is_valid(self, tmp_s):
+        return tmp_s == tmp_s[::-1]
 
 
 if __name__ == "__main__":
-    assert Solution().partition("aab") == [
-        ["aa", "b"],
-        ["a", "a", "b"]
-    ]
+    assert Solution().partition("aab") == [["a", "a", "b"], ["aa", "b"], ]
