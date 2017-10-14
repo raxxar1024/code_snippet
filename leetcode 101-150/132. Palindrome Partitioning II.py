1efee4ca9c090e29223f268836c9f3d23dabbe2b
raxxar1024 <yjw013809@gmail.com>
2017-10-14 11:12:16 +0800

    132. Palindrome Partitioning II

---
+++ leetcode 101-150/132. Palindrome Partitioning II.py
@@ -0,0 +1,21 @@
+"""
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+
+"""
+
+
+class Solution(object):
+    def minCut(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().minCut("aab") == 1
