fab33fac75be50a85a14c1cfa706c9bd5c4dc10a
raxxar1024 <yjw013809@gmail.com>
2017-10-16 15:55:41 +0800

    134. Gas Station

---
+++ leetcode 101-150/134. Gas Station.py
@@ -0,0 +1,26 @@
+"""
+There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1).
+You begin the journey with an empty tank at one of the gas stations.
+
+Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+
+Note:
+The solution is guaranteed to be unique.
+
+"""
+
+
+# Definition for a undirected graph node
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().canCompleteCircuit([1, 2, 3, 4, 5], [2, 3, 4, 5, 1]) == 4
