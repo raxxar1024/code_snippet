08e3cfbe6ec11f8d56e85a2d9f67e1ea2c93e7eb
raxxar1024 <yjw013809@gmail.com>
2017-10-17 17:02:18 +0800

    135. Candy---solution

--- leetcode 101-150/135. Candy.py
+++ leetcode 101-150/135. Candy.py
@@ -16,7 +16,16 @@ class Solution(object):
         :type ratings: List[int]
         :rtype: int
         """
+        candy = [1 for _ in xrange(len(ratings))]
+        for i in xrange(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candy[i] = candy[i - 1] + 1
+        for i in xrange(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1] and candy[i] <= candy[i + 1]:
+                candy[i] = candy[i + 1] + 1
+        return sum(candy)
 
 
 if __name__ == "__main__":
     assert Solution().candy([1, 2, 3, 4, 5]) == 15
+    assert Solution().candy([1, 5, 3, 2, 1, 2, 3, 4, 1]) == 21
