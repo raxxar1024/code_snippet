5ff5e00a553890e1471ec96df2ada4ee595510a0
yjw013809 <yjw013809@gmail.com>
2017-10-18 22:13:21 +0800

    138. Copy List with Random Pointer

---
+++ leetcode 101-150/138. Copy List with Random Pointer.py
@@ -0,0 +1,33 @@
+"""
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+"""
+
+
+# Definition for singly-linked list with a random pointer.
+class RandomListNode(object):
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: RandomListNode
+        :rtype: RandomListNode
+        """
+
+
+if __name__ == "__main__":
+    node_1 = RandomListNode(1)
+    node_2 = RandomListNode(2)
+    node_3 = RandomListNode(3)
+    node_1.next = node_2
+    node_1.random = node_3
+    new_node_1 = Solution().copyRandomList(node_1)
+    assert new_node_1.next.label == 2
+    assert new_node_1.random.label == 3
