4b42784531fa10a941f544b3d5fa229aaddbca7f
yjw013809 <yjw013809@gmail.com>
2017-10-23 22:37:37 +0800

    143. Reorder List

---
+++ leetcode 101-150/143. Reorder List.py
@@ -0,0 +1,39 @@
+"""
+Given a singly linked list L: L0->L1->...->Ln-1->Ln,
+reorder it to: L0->Ln->L1->Ln-1->L2->Ln-2->...
+
+You must do this in-place without altering the nodes' values.
+
+For example,
+Given {1,2,3,4}, reorder it to {1,4,2,3}.
+
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: void Do not return anything, modify head in-place instead.
+        """
+
+
+if __name__ == "__main__":
+    node_1 = ListNode(1)
+    node_2 = ListNode(2)
+    node_3 = ListNode(3)
+    node_4 = ListNode(4)
+    node_1.next = node_2
+    node_1.next.next = node_3
+    node_1.next.next.next = node_4
+    Solution().reorderList(node_1)
+    assert node_1.next == node_4
+    assert node_4.next == node_2
+    assert node_2.next == node_3
