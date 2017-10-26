2f3982491f4c1a3f8d09a63cc893ecce9730495b
yjw013809 <yjw013809@gmail.com>
2017-10-26 22:18:10 +0800

    145. Binary Tree Postorder Traversal

---
+++ leetcode 101-150/145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,40 @@
+"""
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [3,2,1].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+
+if __name__ == "__main__":
+    node_1 = TreeNode(1)
+    node_2 = TreeNode(2)
+    node_3 = TreeNode(3)
+    node_1.right = node_2
+    node_2.left = node_3
+    assert Solution().postorderTraversal(node_1) == [3, 2, 1]
