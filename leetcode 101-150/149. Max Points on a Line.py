0e908e3ea3e2e3ea6d6e0921e61f00bc8b678213
raxxar1024 <yjw013809@gmail.com>
2017-10-31 20:05:05 +0800

    149. Max Points on a Line---solution

--- leetcode 101-150/149. Max Points on a Line.py
+++ leetcode 101-150/149. Max Points on a Line.py
@@ -17,7 +17,32 @@ class Solution(object):
         :type points: List[Point]
         :rtype: int
         """
+        length = len(points)
+        if length < 3:
+            return length
+
+        ret = -1
+        for i in xrange(length):
+            dict_slop = {"inf": 0}
+            same_point_count = 1
+            for j in xrange(length):
+                if i == j:
+                    continue
+                elif points[i].x == points[j].x and points[i].y != points[j].y:
+                    dict_slop["inf"] += 1
+                elif points[i].x != points[j].x:
+                    slop = 0.1 * (points[j].y - points[i].y) / (points[j].x - points[i].x)
+                    if slop in dict_slop:
+                        dict_slop[slop] += 1
+                    else:
+                        dict_slop[slop] = 1
+                else:
+                    same_point_count += 1
+            ret = max(ret, max(dict_slop.values()) + same_point_count)
+
+        return ret
 
 
 if __name__ == "__main__":
-    assert Solution().maxPoints([Point(1, 1), Point(2, 2), Point(3, 4)]) == 2
+    assert Solution().maxPoints([Point(0, 0), Point(94911151, 94911150), Point(94911152, 94911151)]) == 2
+    assert Solution().maxPoints([Point(1, 1), Point(2, 2), Point(3, 3), Point(3, 4)]) == 3
