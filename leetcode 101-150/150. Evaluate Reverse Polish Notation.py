95ac6cbb6309ade79f6e8cc79eed414828389008
raxxar1024 <yjw013809@gmail.com>
2017-11-01 14:24:50 +0800

    150. Evaluate Reverse Polish Notation---solution

--- leetcode 101-150/150. Evaluate Reverse Polish Notation.py
+++ leetcode 101-150/150. Evaluate Reverse Polish Notation.py
@@ -17,7 +17,32 @@ class Solution(object):
         :rtype: int
         """
 
+        operators = ["+", "-", "*", "/"]
+        stack = []
+        for i in xrange(len(tokens)):
+            c = tokens[i]
+            if c in operators:
+                num_1 = stack.pop()
+                num_2 = stack.pop()
+                tmp = 0
+                if c == "+":
+                    tmp = num_2 + num_1
+                elif c == "-":
+                    tmp = num_2 - num_1
+                elif c == "*":
+                    tmp = num_2 * num_1
+                elif c == "/":
+                    tmp = abs(num_2) / abs(num_1)
+                    if num_1 * num_2 < 0:
+                        tmp *= -1
+                stack.append(tmp)
+            else:
+                stack.append(int(tokens[i]))
+
+        return stack[-1]
+
 
 if __name__ == "__main__":
+    assert Solution().evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]) == 22
     assert Solution().evalRPN(["2", "1", "+", "3", "*"]) == 9
     assert Solution().evalRPN(["4", "13", "5", "/", "+"]) == 6
