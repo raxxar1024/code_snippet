935515650af3f01f60a0d0c11bf7ff64b07c84aa
yjw013809 <yjw013809@gmail.com>
2017-11-11 22:24:04 +0800

    162. Find Peak Element---solution

--- leetcode 151-200/162. Find Peak Element.py
+++ leetcode 151-200/162. Find Peak Element.py
@@ -26,6 +26,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        nums = [float("-inf")] + nums + [float("-inf")]
+        for i in xrange(1, len(nums)-1):
+            if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
+                return i - 1
 
 
 if __name__ == "__main__":
