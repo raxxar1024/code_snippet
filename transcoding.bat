9c6f185b5b322c595bc3bcc7369f795b9117f890
yjw013809 <yjw013809@gmail.com>
2017-11-13 16:14:09 +0800

    增加转换wmv格式的部分

--- transcoding.bat
+++ transcoding.bat
@@ -8,7 +8,7 @@ setlocal EnableDelayedExpansion
 set zpath=%CD%
 set prefix=（已转码）
 :GetExt
-set ext=.mp4
+
 set answer=N
 echo.
 echo 您试图将 %zpath%\ 里的所有 %ext% 类型的文件以 %prefix% 为前缀名进行批量改名，是否继续？
@@ -23,16 +23,25 @@ echo 放弃文件改名，按任意键退出... & goto :PauseThenQuit
 set /a num=0
 echo.
  
-
+set ext=.mp4
 for /f "delims=" %%a in ('dir /a /b *%ext%') do (  
 if not exist "已转码" md "已转码"
-ffmpeg.exe -i "%%~a" -s 1024*768 -vcodec h264  -b:v 1024k -r 15 -keyint_min 25 -acodec aac -b:a 48k -ar 24000 -ac 2 "tmp_%%a"
+::ffmpeg -threads 4 -i "zp.mp4" -s 854*480 -vcodec h264  -b:v 360k -r 15 -keyint_min 25 -acodec aac -b:a 48k -ar 24000 -ac 2 "zp_480p_360k.mp4"
+ffmpeg.exe -i "%%~a" -s 854*480 -vcodec h264  -b:v 360k -r 15 -keyint_min 25 -acodec aac -b:a 48k -ar 24000 -ac 2 "tmp_%%a"
 qt-faststart "tmp_%%a" "（已转码）%%a"
 move "（已转码）%%a" "已转码"
 del "tmp_%%a"
 )  
- 
 
+set ext=.wmv
+for /f "delims=" %%a in ('dir /a /b *%ext%') do (  
+if not exist "已转码" md "已转码"
+::ffmpeg -threads 4 -i "zp.mp4" -s 854*480 -vcodec h264  -b:v 360k -r 15 -keyint_min 25 -acodec aac -b:a 48k -ar 24000 -ac 2 "zp_480p_360k.mp4"
+ffmpeg.exe -i "%%~a" -s 854*480 -vcodec h264 -b:v 360k -r 15 -keyint_min 25 -acodec aac -b:a 48k -ar 24000 -ac 2 "tmp_%%~na.mp4"
+qt-faststart "tmp_%%~na.mp4" "（已转码）%%~na.mp4"
+move "（已转码）%%~na.mp4" "已转码"
+del "tmp_%%~na.mp4"
+)  
 
 echo 文件改名完成，按任意键退出...
  
