1526096514393ccc28f7c03ba940726e7945cae5
yjw013809 <yjw013809@gmail.com>
2017-11-13 13:02:14 +0800

    164. Maximum Gap

---
+++ leetcode 151-200/164. Maximum Gap.py
@@ -0,0 +1,25 @@
+"""
+Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+
+Try to solve it in linear time/space.
+
+Return 0 if the array contains less than 2 elements.
+
+You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+
+Credits:
+Special thanks to @porker2008 for adding this problem and creating all test cases.
+
+"""
+
+
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().maximumGap([3, 2, 3, 1, 2, 3]) == 1
