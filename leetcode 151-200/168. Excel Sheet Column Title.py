ffc5708c2998788ee4435014d901fccd9ab44b65
raxxar1024 <yjw013809@gmail.com>
2017-11-16 15:26:14 +0800

    168. Excel Sheet Column Title---solution

--- leetcode 151-200/168. Excel Sheet Column Title.py
+++ leetcode 151-200/168. Excel Sheet Column Title.py
@@ -23,8 +23,20 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
+        result, alphabet = "", []
+        for i in xrange(26):
+            alphabet.append(chr(65 + i))
+
+        n -= 1
+        while n >= 26:
+            result = alphabet[n % 26] + result
+            n = n / 26 - 1
+        result = alphabet[n] + result
+        return result
 
 
 if __name__ == "__main__":
+    assert Solution().convertToTitle(27) == "AA"
+    assert Solution().convertToTitle(52) == "AZ"
     assert Solution().convertToTitle(1) == "A"
     assert Solution().convertToTitle(28) == "AB"
