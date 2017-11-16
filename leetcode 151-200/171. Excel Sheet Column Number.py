dab22315dcbba84f9b4ebf41925b09e5b65de7c2
yjw013809 <yjw013809@gmail.com>
2017-11-16 22:40:29 +0800

    171. Excel Sheet Column Number

---
+++ leetcode 151-200/171. Excel Sheet Column Number.py
@@ -0,0 +1,34 @@
+"""
+Related to question Excel Sheet Column Title
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+
+"""
+
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().titleToNumber("AA") == 27
+    assert Solution().titleToNumber("AZ") == 52
+    assert Solution().titleToNumber("A") == 1
+    assert Solution().titleToNumber("AB") == 28
