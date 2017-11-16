17eebe255143625e9e1bd8842fc6d5d43eb9de23
yjw013809 <yjw013809@gmail.com>
2017-11-16 22:47:51 +0800

    171. Excel Sheet Column Number---solution

--- leetcode 151-200/171. Excel Sheet Column Number.py
+++ leetcode 151-200/171. Excel Sheet Column Number.py
@@ -25,6 +25,10 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        result = 0
+        for c in s:
+            result = result * 26 + ord(c) - ord("A") + 1
+        return result
 
 
 if __name__ == "__main__":
