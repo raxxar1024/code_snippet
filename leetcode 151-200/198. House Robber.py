ef60d659ce4caebaa172b3a73c2026b221e6e1eb
raxxar1024 <yjw013809@gmail.com>
2017-11-22 14:17:45 +0800

    198. House Robber---solution but TLE

--- leetcode 151-200/198. House Robber.py
+++ leetcode 151-200/198. House Robber.py
@@ -19,6 +19,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return max(nums[0], nums[1])
+        elif len(nums) == 3:
+            return max(nums[0]+nums[2], nums[1])
+        else:
+            return max(nums[0] + self.rob(nums[2:]), nums[1] + self.rob(nums[3:]))
 
 
 if __name__ == "__main__":
