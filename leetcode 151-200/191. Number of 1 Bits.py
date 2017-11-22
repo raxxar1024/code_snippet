e2a1d3d33c722c97cf37e09f6fc82c9da73126ce
raxxar1024 <yjw013809@gmail.com>
2017-11-22 08:55:11 +0800

    191. Number of 1 Bits---solution

--- leetcode 151-200/191. Number of 1 Bits.py
+++ leetcode 151-200/191. Number of 1 Bits.py
@@ -15,6 +15,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        count = 0
+        while n > 0:
+            if n & 1 == 1:
+                count += 1
+            n >>= 1
+        return count
 
 
 if __name__ == "__main__":
