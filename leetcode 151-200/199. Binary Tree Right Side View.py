deca3c84c15480ee7169dd8cdd50bec9fc69c567
raxxar1024 <yjw013809@gmail.com>
2017-11-23 09:03:55 +0800

    199. Binary Tree Right Side View---solution

--- leetcode 151-200/199. Binary Tree Right Side View.py
+++ leetcode 151-200/199. Binary Tree Right Side View.py
@@ -31,6 +31,25 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
+        if not root:
+            return []
+
+        level_1 = [root]
+
+        def get_next_level(level):
+            next_level = []
+            for item in level:
+                if item.left:
+                    next_level.append(item.left)
+                if item.right:
+                    next_level.append(item.right)
+            return next_level
+
+        result = []
+        while level_1:
+            result.append(level_1[-1].val)
+            level_1 = get_next_level(level_1)
+        return result
 
 
 if __name__ == "__main__":
@@ -39,4 +58,10 @@ if __name__ == "__main__":
     root.right = TreeNode(3)
     root.right.right = TreeNode(4)
     root.left.right = TreeNode(5)
+    assert Solution().rightSideView(root) == [1, 3, 4]
+
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
     assert Solution().rightSideView(root) == [1, 3, 4]
