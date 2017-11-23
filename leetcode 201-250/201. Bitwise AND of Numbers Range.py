d8146d920f9decd1eb6bfe988e902d07bb78eb76
raxxar1024 <yjw013809@gmail.com>
2017-11-23 10:04:25 +0800

    201. Bitwise AND of Numbers Range

---
+++ leetcode 201-250/201. Bitwise AND of Numbers Range.py
@@ -0,0 +1,22 @@
+"""
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+For example, given the range [5, 7], you should return 4.
+
+Credits:
+Special thanks to @amrsaqr for adding this problem and creating all test cases.
+
+"""
+
+
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().rangeBitwiseAnd(5, 7) == 4
