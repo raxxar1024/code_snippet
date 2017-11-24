e9a3149fd47b7d7058d1c099e0964c4e045501cf
raxxar1024 <yjw013809@gmail.com>
2017-11-24 08:51:13 +0800

    201. Bitwise AND of Numbers Range---solution

--- leetcode 201-250/201. Bitwise AND of Numbers Range.py
+++ leetcode 201-250/201. Bitwise AND of Numbers Range.py
@@ -16,6 +16,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        count = 0
+        while m != n:
+            m &= (0x7FFFFFFF << count)
+            n &= (0x7FFFFFFF << count)
+            count += 1
+        return m
 
 
 if __name__ == "__main__":
