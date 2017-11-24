3d87d53c3965d1f0f678eddf65e548bc5b49303c
raxxar1024 <yjw013809@gmail.com>
2017-11-24 17:55:44 +0800

    204. Count Primes

---
+++ leetcode 201-250/204. Count Primes.py
@@ -0,0 +1,21 @@
+"""
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+
+Credits:
+Special thanks to @mithmatt for adding this problem and creating all test cases.
+
+"""
+
+
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().countPrimes(18) == 7
