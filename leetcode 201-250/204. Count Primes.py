730c57d03744753ed701d738a4a3559f53e0a037
yjw013809 <yjw013809@gmail.com>
2017-11-25 19:20:35 +0800

    204. Count Primes---solution

--- leetcode 201-250/204. Count Primes.py
+++ leetcode 201-250/204. Count Primes.py
@@ -15,6 +15,20 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        flag = [True for _ in xrange(n)]
+
+        for i in xrange(2, n / 2 + 1):
+            if flag[i] is True:
+                tmp = 2 * i
+                while tmp < n:
+                    flag[tmp] = False
+                    tmp += i
+
+        count = 0
+        for i in xrange(2, n):
+            if flag[i] is True:
+                count += 1
+        return count
 
 
 if __name__ == "__main__":
