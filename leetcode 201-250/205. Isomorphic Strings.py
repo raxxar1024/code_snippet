124fee0179d58a946944e6063fe53288b1d2ab56
yjw013809 <yjw013809@gmail.com>
2017-11-26 22:45:47 +0800

    205. Isomorphic Strings---solution

--- leetcode 201-250/205. Isomorphic Strings.py
+++ leetcode 201-250/205. Isomorphic Strings.py
@@ -25,6 +25,19 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        flag_s = ["0" for _ in xrange(len(s))]
+        flag_t = ["0" for _ in xrange(len(t))]
+        dict_s = {}
+        dict_t = {}
+
+        for i in xrange(len(s)):
+            if s[i] not in dict_s:
+                dict_s[s[i]] = str(i)
+            flag_s[i] = dict_s[s[i]]
+            if t[i] not in dict_t:
+                dict_t[t[i]] = str(i)
+            flag_t[i] = dict_t[t[i]]
+        return flag_s == flag_t
 
 
 if __name__ == "__main__":
