ff6dadbc9e7a04247de53bd9dbd1db0bb3a22cbe
yjw013809 <yjw013809@gmail.com>
2017-11-29 23:00:05 +0800

    208. Implement Trie (Prefix Tree)---solution

--- leetcode 201-250/208. Implement Trie (Prefix Tree).py
+++ leetcode 201-250/208. Implement Trie (Prefix Tree).py
@@ -7,11 +7,18 @@ You may assume that all inputs are consist of lowercase letters a-z.
 """
 
 
+class TrieNode(object):
+    def __init__(self):
+        self.is_string = False
+        self.leaves = {}
+
+
 class Trie(object):
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        self.root = TrieNode()
 
     def insert(self, word):
         """
@@ -19,6 +26,12 @@ class Trie(object):
         :type word: str
         :rtype: void
         """
+        curr = self.root
+        for c in word:
+            if c not in curr.leaves:
+                curr.leaves[c] = TrieNode()
+            curr = curr.leaves[c]
+        curr.is_string = True
 
     def search(self, word):
         """
@@ -26,6 +39,11 @@ class Trie(object):
         :type word: str
         :rtype: bool
         """
+        node = self.child_search(word)
+        if node:
+            return node.is_string
+        else:
+            return False
 
     def startsWith(self, prefix):
         """
@@ -33,6 +51,16 @@ class Trie(object):
         :type prefix: str
         :rtype: bool
         """
+        return self.child_search(prefix) is not None
+
+    def child_search(self, word):
+        curr = self.root
+        for c in word:
+            if c in curr.leaves:
+                curr = curr.leaves[c]
+            else:
+                return None
+        return curr
 
 
 if __name__ == "__main__":
