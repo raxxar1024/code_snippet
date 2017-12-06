3e0071e8bda63982fa09152611a73bcfded1ea9a
raxxar1024 <yjw013809@gmail.com>
2017-12-06 08:53:33 +0800

    214. Shortest Palindrome

---
+++ leetcode 201-250/214. Shortest Palindrome.py
@@ -0,0 +1,28 @@
+"""
+Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it.
+Find and return the shortest palindrome you can find by performing this transformation.
+
+For example:
+
+Given "aacecaaa", return "aaacecaaa".
+
+Given "abcd", return "dcbabcd".
+
+Credits:
+Special thanks to @ifanchu for adding this problem and creating all test cases.
+Thanks to @Freezen for additional test cases.
+
+"""
+
+
+class Solution(object):
+    def shortestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().shortestPalindrome("aacecaaa") == "aaacecaaa"
+    assert Solution().shortestPalindrome("abcd") == "dcbabcd"
