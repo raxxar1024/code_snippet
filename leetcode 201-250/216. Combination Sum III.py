9c5ffb935952fadc546ef07f1fb78e4dba483b9c
yjw013809 <yjw013809@gmail.com>
2017-12-10 21:52:14 +0800

    216. Combination Sum III---solution but TLE

--- leetcode 201-250/216. Combination Sum III.py
+++ leetcode 201-250/216. Combination Sum III.py
@@ -30,8 +30,37 @@ class Solution(object):
         :type n: int
         :rtype: List[List[int]]
         """
+        import copy
+        result = []
+        nums = [i for i in xrange(1, 10)]
+
+        def combination_recu(tmp_k, tmp_n, candidates, intermediate):
+            if tmp_k == 0:
+                if tmp_n == 0:
+                    intermediate.sort()
+                    if intermediate not in result:
+                        tmp = copy.deepcopy(intermediate)
+                        result.append(tmp)
+                else:
+                    return
+            if tmp_k < 0 or tmp_n < 0:
+                return
+            for candidate in candidates:
+                tmp = copy.deepcopy(candidates)
+                tmp.remove(candidate)
+                intermediate.append(candidate)
+                combination_recu(tmp_k - 1, tmp_n - candidate, tmp, intermediate)
+                intermediate.remove(candidate)
+
+        combination_recu(k, n, nums, [])
+        return result
 
 
 if __name__ == "__main__":
+    assert Solution().combinationSum3(3, 8) == [[1, 2, 3, 4, 5, 6, 7, 8]]
+    assert Solution().combinationSum3(3, 15) == [
+        [1, 5, 9], [1, 6, 8], [2, 4, 9], [2, 5, 8], [2, 6, 7], [3, 4, 8],
+        [3, 5, 7], [4, 5, 6]
+    ]
     assert Solution().combinationSum3(3, 7) == [[1, 2, 4]]
     assert Solution().combinationSum3(3, 9) == [[1, 2, 6], [1, 3, 5], [2, 3, 4]]
