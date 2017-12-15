c33c875f1dee0f15368e4c4052113578ceaebb7b
yjw013809 <yjw013809@gmail.com>
2017-12-15 23:18:08 +0800

    221. Maximal Square

---
+++ leetcode 201-250/221. Maximal Square.py
@@ -0,0 +1,31 @@
+"""
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+For example, given the following matrix:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+Return 4.
+Credits:
+Special thanks to @Freezen for adding this problem and creating all test cases.
+
+"""
+
+
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().maximalSquare([
+        [1, 0, 1, 0, 0],
+        [1, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 1, 0],
+    ]) == 4
