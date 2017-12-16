99f411760a67a49359f58fe165921b667e2c3009
raxxar1024 <yjw013809@gmail.com>
2017-12-16 15:32:51 +0800

    221. Maximal Square---solution

--- leetcode 201-250/221. Maximal Square.py
+++ leetcode 201-250/221. Maximal Square.py
@@ -20,12 +20,33 @@ class Solution(object):
         :type matrix: List[List[str]]
         :rtype: int
         """
+        if not matrix:
+            return 0
+
+        dp = [[[0, 0] for _ in xrange(len(matrix[0]) + 1)] for _ in xrange(len(matrix) + 1)]
+        for i in xrange(1, len(matrix) + 1):
+            for j in xrange(1, len(matrix[0]) + 1):
+                if matrix[i - 1][j - 1] == "1":
+                    dp[i][j][0] = dp[i][j - 1][0] + 1
+                    dp[i][j][1] = dp[i - 1][j][1] + 1
+
+        side = [[0 for _ in xrange(len(matrix[0]))] for _ in xrange(len(matrix))]
+        max_square = 0
+        for i in xrange(len(matrix)):
+            for j in xrange(len(matrix[0])):
+                side[i][j] = min(dp[i + 1][j + 1][0], dp[i + 1][j + 1][1])
+                if matrix[i][j] == "1":
+                    if i > 0 and j > 0:
+                        side[i][j] = min(side[i][j], side[i - 1][j - 1] + 1)
+                max_square = max(max_square, side[i][j] * side[i][j])
+
+        return max_square
 
 
 if __name__ == "__main__":
     assert Solution().maximalSquare([
-        [1, 0, 1, 0, 0],
-        [1, 0, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 0, 0, 1, 0],
+        ["1", "0", "1", "0", "0"],
+        ["1", "0", "1", "1", "1"],
+        ["1", "1", "1", "1", "1"],
+        ["1", "0", "0", "1", "0"],
     ]) == 4
