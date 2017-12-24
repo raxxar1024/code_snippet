38ed70b87365592c1f68fa469f4eeb5495bada7a
yjw013809 <yjw013809@gmail.com>
2017-12-24 23:00:57 +0800

    228. Summary Ranges

---
+++ leetcode 201-250/228. Summary Ranges.py
@@ -0,0 +1,26 @@
+"""
+Given a sorted integer array without duplicates, return the summary of its ranges.
+
+Example 1:
+Input: [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Example 2:
+Input: [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+
+"""
+
+
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().summaryRanges([0, 1, 2, 4, 5, 7]) == ["0->2", "4->5", "7"]
+    assert Solution().summaryRanges([0, 2, 3, 4, 6, 8, 9]) == ["0", "2->4", "6", "8->9"]
