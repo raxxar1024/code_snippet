fbcbe26971e0abff99706ffb891027d751cae3e4
raxxar1024 <yjw013809@gmail.com>
2017-12-25 15:37:41 +0800

    224. Basic Calculator---solution

--- leetcode 201-250/224. Basic Calculator.py
+++ leetcode 201-250/224. Basic Calculator.py
@@ -21,9 +21,36 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        operators, operands = [], []
+        operand = ""
+        for i in reversed(xrange(len(s))):
+            if s[i].isdigit():
+                operand += s[i]
+                if i == 0 or not s[i - 1].isdigit():
+                    operands.append(int(operand[::-1]))
+                    operand = ""
+            elif s[i] in ["+", "-", ")"]:
+                operators.append(s[i])
+            elif s[i] == "(":
+                while operators[-1] != ")":
+                    self.compute(operators, operands)
+                operators.pop()
+
+        while operators:
+            self.compute(operators, operands)
+        return operands[-1]
+
+    def compute(self, operators, operands):
+        left, right = operands.pop(), operands.pop()
+        op = operators.pop()
+        if op == "+":
+            operands.append(left + right)
+        elif op == "-":
+            operands.append(left - right)
 
 
 if __name__ == "__main__":
+    assert Solution().calculate("0") == 0
     assert Solution().calculate("1 + 1") == 2
     assert Solution().calculate(" 2-1 + 2 ") == 3
     assert Solution().calculate("(1+(4+5+2)-3)+(6+8)") == 23
