ee523c0f50add23bb4b4bd209cfd0656b07e07ee
raxxar1024 <yjw013809@gmail.com>
2017-12-27 08:57:38 +0800

    231. Power of Two

---
+++ leetcode 201-250/231. Power of Two.py
@@ -0,0 +1,20 @@
+"""
+Given an integer, write a function to determine if it is a power of two.
+
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+
+"""
+
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+
+if __name__ == "__main__":
+    assert Solution().isPowerOfTwo(4) is True
+    assert Solution().isPowerOfTwo(5) is False
