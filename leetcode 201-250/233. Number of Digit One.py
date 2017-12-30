78d6aab5a24fe8f15f04c3c5149f3cf50faa8a36
raxxar1024 <yjw013809@gmail.com>
2017-12-30 15:25:47 +0800

    233. Number of Digit One---solution

--- leetcode 201-250/233. Number of Digit One.py
+++ leetcode 201-250/233. Number of Digit One.py
@@ -14,6 +14,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        res, a, b = 0, 1, 1
+        while n > 0:
+            res += (n + 8) / 10 * a + (n % 10 == 1) * b
+            b += n % 10 * a
+            a *= 10
+            n /= 10
+        return res
 
 
 if __name__ == "__main__":
