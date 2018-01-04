0c15646ad48f1f9490d51a59faea22b76d331c84
yjw013809 <yjw013809@gmail.com>
2018-01-04 22:06:08 +0800

    238. Product of Array Except Self---solution

--- leetcode 201-250/238. Product of Array Except Self.py
+++ leetcode 201-250/238. Product of Array Except Self.py
@@ -19,6 +19,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        if not nums:
+            return []
+
+        left_product = [1 for _ in xrange(len(nums))]
+        for i in xrange(1, len(nums)):
+            left_product[i] *= left_product[i - 1] * nums[i - 1]
+
+        right_product = [1 for _ in xrange(len(nums))]
+        for i in xrange(len(nums) - 2, -1, -1):
+            right_product[i] = right_product[i + 1] * nums[i + 1]
+
+        return [left_product[i] * right_product[i] for i in xrange(len(nums))]
 
 
 if __name__ == "__main__":
