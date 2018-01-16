5d8e1ca65bda53dfafaf6bf6f5f7a6868a62307f
raxxar1024 <yjw013809@gmail.com>
2018-01-16 11:43:38 +0800

    273. Integer to English Words---elegant solution

--- leetcode 251-300/273. Integer to English Words.py
+++ leetcode 251-300/273. Integer to English Words.py
@@ -18,39 +18,33 @@ class Solution(object):
         """
         if num == 0:
             return "Zero"
-        less_than_20 = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-                        "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
-                        "Seventeen", "Eighteen", "Nineteen"]
-        tens = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        lookups = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine",
+                   10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen",
+                   17: "Seventeen", 18: "Eighteen", 19: "Nineteen", 20: "Twenty", 30: "Thirty", 40: "Forty",
+                   50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty", 90: "Ninety"}
+        unit = ["", "Thousand", "Million", "Billion"]
+        res, i = [], 0
+        while num > 0:
+            if num % 1000:
+                res.append(self.three_digits(num % 1000, lookups, unit[i]))
+            i += 1
+            num /= 1000
+        return " ".join(res[::-1])
 
-        def numberToWords_3bit(num):
-            if num == 0:
-                return ""
-            tmp = ""
-            if num >= 100:
-                tmp += less_than_20[num / 100 - 1] + " Hundred "
-                num %= 100
-            if num > 19:
-                tmp += tens[num / 10 - 2]
-                if num % 10 != 0:
-                    tmp += " %s" % less_than_20[num % 10 - 1]
-            elif 0 < num < 20:
-                tmp += less_than_20[num - 1]
-            return tmp[:-1] if tmp[-1] == " " else tmp
+    def three_digits(self, num, lookups, unit):
+        res = []
+        if num // 100:
+            res.append(lookups[num // 100] + " Hundred")
+        if num % 100:
+            res.append(self.two_digits(num % 100, lookups))
+        if unit != "":
+            res.append(unit)
+        return " ".join(res)
 
-        tmp = ""
-        if num >= 1000000000:
-            tmp += "%s Billion " % numberToWords_3bit(num / 1000000000)
-            num %= 1000000000
-        if num >= 1000000:
-            tmp += "%s Million " % numberToWords_3bit(num / 1000000)
-            num %= 1000000
-        if num >= 1000:
-            tmp += "%s Thousand " % numberToWords_3bit(num / 1000)
-            num %= 1000
-        tmp += numberToWords_3bit(num)
-
-        return tmp[:-1] if tmp[-1] == " " else tmp
+    def two_digits(self, num, lookups):
+        if num in lookups:
+            return lookups[num]
+        return lookups[num // 10 * 10] + " " + lookups[num % 10]
 
 
 if __name__ == "__main__":
