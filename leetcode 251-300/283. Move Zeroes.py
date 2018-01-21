5e79e4301e8d798d79dda025b6376ca2f8b6d362
yjw013809 <yjw013809@gmail.com>
2018-01-21 22:06:56 +0800

    283. Move Zeroes---solution

--- leetcode 251-300/283. Move Zeroes.py
+++ leetcode 251-300/283. Move Zeroes.py
@@ -20,9 +20,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        pos = 0
+        for i in xrange(len(nums)):
+            if nums[i]:
+                nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
 
 
 if __name__ == "__main__":
     nums = [0, 1, 0, 3, 12]
-    Solution().moveZeroes()
+    Solution().moveZeroes(nums)
     assert nums == [1, 3, 12, 0, 0]
