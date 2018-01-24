b123e741bc2ca8176aa8871cef909f42439d3e67
yjw013809 <yjw013809@gmail.com>
2018-01-24 22:01:49 +0800

    287. Find the Duplicate Number---solution

--- leetcode 251-300/287. Find the Duplicate Number.py
+++ leetcode 251-300/287. Find the Duplicate Number.py
@@ -20,6 +20,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        finder = 0
+        while finder != slow:
+            finder = nums[finder]
+            slow = nums[slow]
+
+        return slow
 
 
 if __name__ == "__main__":
