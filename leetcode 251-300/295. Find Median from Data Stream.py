e8955dba1ad8a5828fcaac70deded22fac2b0726
raxxar1024 <yjw013809@gmail.com>
2018-01-29 14:28:52 +0800

    295. Find Median from Data Stream---solution

--- leetcode 251-300/295. Find Median from Data Stream.py
+++ leetcode 251-300/295. Find Median from Data Stream.py
@@ -25,37 +25,45 @@ Special thanks to @Louis1992 for adding this problem and creating all test cases
 
 """
 
+from heapq import heappop, heappush
+
 
 class MedianFinder(object):
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.nums = []
-        self.length = 0
+        self.small_part = []
+        self.big_part = []
 
     def addNum(self, num):
         """
         :type num: int
         :rtype: void
         """
-        self.nums.append(num)
-        self.nums.sort()
-        self.length += 1
+        if not self.big_part or num > self.big_part[0]:
+            heappush(self.big_part, num)
+            if len(self.big_part) > len(self.small_part) + 1:
+                heappush(self.small_part, -heappop(self.big_part))
+        else:
+            heappush(self.small_part, -num)
+            if len(self.small_part) > len(self.big_part):
+                heappush(self.big_part, -heappop(self.small_part))
 
     def findMedian(self):
         """
         :rtype: float
         """
-        if self.length % 2 == 0:
-            return (self.nums[self.length / 2] + self.nums[self.length / 2 - 1]) / 2.0
+        if len(self.small_part) == len(self.big_part):
+            return (self.big_part[0] + (-self.small_part[0])) / 2.0
         else:
-            return self.nums[self.length / 2]
+            return self.big_part[0]
 
 
 if __name__ == "__main__":
     obj = MedianFinder()
     obj.addNum(1)
+    assert obj.findMedian() == 1
     obj.addNum(2)
     assert obj.findMedian() == 1.5
     obj.addNum(3)
