3c151c0af5edb85d5aed444d37fdf67ba3d83d3d
raxxar1024 <yjw013809@gmail.com>
2018-02-02 10:13:25 +0800

    300. Longest Increasing Subsequence---o(n2) solution

--- leetcode 251-300/300. Longest Increasing Subsequence.py
+++ leetcode 251-300/300. Longest Increasing Subsequence.py
@@ -22,6 +22,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        dp = []
+        for i in range(len(nums)):
+            dp.append(1)
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp) if dp else 0
 
 
 if __name__ == "__main__":
