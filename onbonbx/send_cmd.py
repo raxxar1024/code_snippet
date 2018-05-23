f862936cb03ef731853a41353ce415e2ab1efb33
raxxar1024 <yjw013809@gmail.com>
2018-05-23 16:34:31 +0800

    optimize send_cmd.py

--- onbonbx/send_cmd.py
+++ onbonbx/send_cmd.py
@@ -20,14 +20,25 @@ def send_cmd(cmd):
     tcpCliSock.close()
 
 
+# A5 A5 A5 A5 A5 A5 A5 A5
+# fe ff 00 80 f0 00 fe ff 00 00 00 00 0E 00 00 00
+# 01 a9 01 00 00 50 30 30 30 00 00 01 00 01
+# 3C 65
+# 5a
+import struct
+
 if __name__ == "__main__":
     RtnReq = 0x01
     CmdGroup = 0xA9
     Cmd = 0x01
-    Reserved = 0x0000
-    BlockUnit = 0x1
-    import struct
+    Reserved = 0x0
+    BlockUnit = "P000"
 
-    cmd = struct.pack(">BBBHB", RtnReq, CmdGroup, Cmd, Reserved, BlockUnit)
-    print " ".join([hex(ord(x)) for x in cmd])
-    send_cmd(cmd)
+    part_1 = struct.pack("QHHBBHIIBBBBB4s", 0xA5A5A5A5A5A5A5A5, 0xfffe, 0x8000, 0xf0, 0x00, 0xfffe, 0x0, 0x0e,
+                         RtnReq, CmdGroup, Cmd, Reserved, Reserved, BlockUnit)
+    print " ".join([hex(ord(x)) for x in part_1])
+
+    # crc = 1
+    # cmd = struct.pack("8B16B14B", frame_header, packet_header, data, crc, 0x5A)
+    # print " ".join([hex(ord(x)) for x in cmd])
+    # send_cmd(cmd)
